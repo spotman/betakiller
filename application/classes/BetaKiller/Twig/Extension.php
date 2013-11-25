5e755ffeda328623be533971858832eea817c396
Spotman <i.am@spotman.ru>
2013-11-25 23:20:46 +0400

    static files refactoring + twig configured

---
+++ application/classes/BetaKiller/Twig/Extension.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class BetaKiller_Twig_Extension extends Twig_Extension {
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'BetaKiller';
+    }
+
+
+}
