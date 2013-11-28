1c9339f593cfaf12a9486ee8e48ce01283f4e9f0
Spotman <i.am@spotman.ru>
2013-11-28 14:36:03 +0400

    Better Twig view implementation and small fixes

---
+++ application/i18n/ru.php
@@ -0,0 +1,13 @@
+<?php defined("SYSPATH") OR die("No direct script access.");
+/**
+ * Translation file in language: ru
+ * Automatically generated from previous translation file.
+ */
+return array (
+  'The requested view :file could not be found' => 'The requested view :file could not be found',
+  'arguments' => 'arguments',
+  'PHP internal call' => 'PHP internal call',
+  'Environment' => 'Environment',
+  'Included files' => 'Included files',
+  'Loaded extensions' => 'Loaded extensions',
+);
