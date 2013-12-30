64d691618d38111bb78b7f2abe00b76e4ff5f637
Spotman <i.am@spotman.ru>
2013-12-30 14:39:32 +0400

    Working on AFS

--- application/classes/ORM.php
+++ application/classes/ORM.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 
-abstract class ORM extends Kohana_ORM implements API_Model_Response_Item, DataSource_Interface {
+class ORM extends Kohana_ORM implements API_Model_Response_Item, DataSource_Interface {
 
     public function belongs_to(array $config = NULL)
     {
