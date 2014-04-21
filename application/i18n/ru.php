23b062cee176a15e0f1e56727e8679652024b606
Spotman <i.am@spotman.ru>
2014-04-21 17:57:45 +0400

    small fixes

--- application/i18n/ru.php
+++ application/i18n/ru.php
@@ -4,8 +4,6 @@
  * Automatically generated from previous translation file.
  */
 return array (
-  'Default value:' => 'Default value:',
-  ':error' => ':error',
   'Empty layer for :codename IFace' => 'Empty layer for :codename IFace',
   'Not implemented yet' => 'Not implemented yet',
   'Error reading session data.' => 'Error reading session data.',
