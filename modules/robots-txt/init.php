f8bd6453d58f3537c3836c4154c2c90d9a14c0ba
Spotman <i.am@spotman.ru>
2014-06-18 16:56:42 +0400

    update submodules + new robots.txt module

--- modules/robots-txt/init.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-Route::set('robots-txt', 'robots.txt')
-    ->defaults(array(
-        'controller' => 'RobotsTXT',
-        'action' => 'index'
-	));
