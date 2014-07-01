b86f5d86aa371c3a280e1f309ec8442b92bf823d
Spotman <i.am@spotman.ru>
2014-07-01 16:08:46 +0400

    Meta tags: author and url

---
+++ application/config/meta.php
@@ -0,0 +1,52 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+	// Default template(View) for render tags
+	'template' => 'meta/full',
+	// HTTP-EQUIV group tags 
+	'http-equiv' => array(
+		'resource-type',
+		'content-language', 
+		'content-script-type', 
+		'content-style-type', 
+		'content-type', 
+		'expires', 
+		'pics-label', 
+		'pragma', 
+		'refresh', 
+		'set-cookie', 
+		'window-target',
+		'x-ua-compatible',
+		'window-target',
+	),
+	// Auto load tags from config groups. Ex: array('meta.tags', 'site_meta', 'blog.meta_tags')
+	'tags_config_groups' => array('meta.tags'),
+	// Default tag values. See http://en.wikipedia.org/wiki/Meta_element
+	'tags' => array(
+		'title'				=> NULL,
+		'description'		=> NULL,
+		'abstract'			=> NULL,
+		'keywords'			=> NULL,
+		'author'			=> 'Spotman',
+		'copyright'			=> 'http://spotman.ru/',
+		'generator'			=> 'BetaKiller',
+		'subject'			=> NULL,
+		'url'				=> NULL,
+		'rating'			=> 'general',
+		'audience'			=> 'all',
+		'document-state'	=> 'dynamic',
+		'revisit'			=> '3 days',
+		'revisit-after'		=> '3 days',
+		'robots'			=> 'all',
+		'content-type'		=> Kohana::$content_type.'; charset='.Kohana::$charset,
+		'content-language'	=> I18n::$lang,
+		'x-ua-compatible'	=> 'IE=edge,chrome=1',
+		'viewport'			=> 'width=device-width,initial-scale=1.0',
+	),
+	// Cache lifetime in seconds. Default value = 300(5 minutes)
+	'cache_lifetime' => (Kohana::$caching ? 300 : 0), 
+	// Add slash at end of tag? Set as FALSE for HTML5
+	'slash_at_end' => TRUE, 
+	// A separator is added between the parts of the title array
+	'title_separator' => ' - ',
+);
