0ae3e161612316b7596fff2a56fa80e9ec6971a8
Spotman <i.am@spotman.ru>
2015-10-15 16:03:56 +0300

    update backup config path (include deployer`s shared directories and all releases in production/staging env)

---
+++ application/config/environments/staging/backup.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+
+    // Include all deployer`s directories
+    'folder'    =>  MultiSite::instance()->site_path().'/../../../',
+
+);
