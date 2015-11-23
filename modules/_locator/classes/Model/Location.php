35636d111cb2106302c04ee0f4de7fafe940484a
Spotman <i.am@spotman.ru>
2015-11-23 14:35:05 +0300

    cleanup old unused modules

--- modules/_locator/classes/Model/Location.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-/**
- * Class Model_Location
- * @category   Models
- * @author     Spotman
- * @package    Betakiller
- */
-class Model_Location extends Model_Kohana_Location {}
--- modules/_locator/init.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-// Make system routes for current configuration
-// Locator::init_routes();
