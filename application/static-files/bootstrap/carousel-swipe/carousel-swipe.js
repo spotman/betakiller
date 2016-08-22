cee3acafb3541d3a89e08ae4dce6aad50a76f42c
Spotman <i.am@spotman.ru>
2016-08-22 22:29:51 +0300

    bootstrap-swipe lib fixes + jquery word counter lib

--- application/static-files/bootstrap/carousel-swipe/carousel-swipe.js
+++ application/static-files/bootstrap/carousel-swipe/carousel-swipe.js
@@ -9,9 +9,9 @@
   // =========================
 
   var CarouselSwipe = function(element) {
-    this.$element    = $(element)
-    this.carousel    = this.$element.data('bs.carousel')
-    this.options     = $.extend({}, CarouselSwipe.DEFAULTS, this.carousel.options)
+    this.$element    = $(element);
+    this.carousel    = this.$element.data('bs.carousel');
+    this.options     = $.extend({}, CarouselSwipe.DEFAULTS, this.carousel.options);
     this.startX      =
     this.startY      =
     this.startTime   =
@@ -20,8 +20,8 @@
     this.$items      =
     this.$next       =
     this.$prev       =
-    this.dx          = null
-    this.sliding     = false
+    this.dx          = null;
+    this.sliding     = false;
 
     this.$element
       .on('touchstart',        $.proxy(this.touchstart,this))
@@ -29,82 +29,82 @@
       .on('touchend',          $.proxy(this.touchend,this))
       .on('slide.bs.carousel', $.proxy(this.sliding, this))
       .on('slid.bs.carousel',  $.proxy(this.stopSliding, this))
-  }
+  };
 
   CarouselSwipe.DEFAULTS = {
     swipe: 50 // percent per second
-  }
+  };
 
   CarouselSwipe.prototype.sliding = function() {
     this.sliding = true
-  }
+  };
 
   CarouselSwipe.prototype.stopSliding = function() {
     this.sliding = false
-  }
+  };
 
   CarouselSwipe.prototype.touchstart = function(e) {
     if (this.sliding || !this.options.swipe) return;
-    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e
-    this.dx = 0
-    this.startX = touch.pageX
-    this.startY = touch.pageY
-    this.cycling = null
-    this.width = this.$element.width()
+    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e;
+    this.dx = 0;
+    this.startX = touch.pageX;
+    this.startY = touch.pageY;
+    this.cycling = null;
+    this.width = this.$element.width();
     this.startTime = e.timeStamp
-  }
+  };
 
   CarouselSwipe.prototype.touchmove = function(e) {
     if (this.sliding || !this.options.swipe) return;
-    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e
-    var dx = touch.pageX - this.startX
-    var dy = touch.pageY - this.startY
+    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e;
+    var dx = touch.pageX - this.startX;
+    var dy = touch.pageY - this.startY;
     if (Math.abs(dx) < Math.abs(dy)) return; // vertical scroll
 
     if ( this.cycling === null ) {
-      this.cycling = !!this.carousel.interval
-      this.cycling && this.carousel.pause()
+      this.cycling = !!this.carousel.interval;
+      this.cycling && this.carousel.pause();
     }
 
-    e.preventDefault()
-    this.dx = dx / (this.width || 1) * 100
+    e.preventDefault();
+    this.dx = dx / (this.width || 1) * 100;
     this.swipe(this.dx)
-  }
+  };
 
   CarouselSwipe.prototype.touchend = function(e) {
     if (this.sliding || !this.options.swipe) return;
     if (!this.$active) return; // nothing moved
     var all = $()
       .add(this.$active).add(this.$prev).add(this.$next)
-      .carousel_transition(true)
+      .carousel_transition(true);
 
-    var dt = (e.timeStamp - this.startTime) / 1000
-    var speed = Math.abs(this.dx / dt) // percent-per-second
+    var dt = (e.timeStamp - this.startTime) / 1000;
+    var speed = Math.abs(this.dx / dt); // percent-per-second
     if (this.dx > 40 || (this.dx > 0 && speed > this.options.swipe)) {
-      this.carousel.prev()
+      this.carousel.prev();
     } else if (this.dx < -40 || (this.dx < 0 && speed > this.options.swipe)) {
       this.carousel.next();
     } else {
       this.$active
         .one($.support.transition.end, function () {
-          all.removeClass('prev next')
+          all.removeClass('prev next');
         })
-      .emulateTransitionEnd(this.$active.css('transition-duration').slice(0, -1) * 1000)
+      .emulateTransitionEnd(this.$active.css('transition-duration').slice(0, -1) * 1000);
     }
 
-    all.css('transform', '')
-    this.cycling && this.carousel.cycle()
-    this.$active = null // reset the active element
-  }
+    all.css('transform', '');
+    this.cycling && this.carousel.cycle();
+    this.$active = null; // reset the active element
+  };
 
   CarouselSwipe.prototype.swipe = function(percent) {
-    var $active = this.$active || this.getActive()
+    var $active = this.$active || this.getActive();
     if (percent < 0) {
         this.$prev
             .css('transform', 'translate3d(0,0,0)')
             .removeClass('prev')
-            .carousel_transition(true)
-        if (!this.$next.length || this.$next.hasClass('active')) return
+            .carousel_transition(true);
+        if (!this.$next.length || this.$next.hasClass('active')) return;
         this.$next
             .carousel_transition(false)
             .addClass('next')
@@ -113,48 +113,48 @@
         this.$next
             .css('transform', '')
             .removeClass('next')
-            .carousel_transition(true)
-        if (!this.$prev.length || this.$prev.hasClass('active')) return
+            .carousel_transition(true);
+        if (!this.$prev.length || this.$prev.hasClass('active')) return;
         this.$prev
             .carousel_transition(false)
             .addClass('prev')
-            .css('transform', 'translate3d(' + (percent - 100) + '%,0,0)')
+            .css('transform', 'translate3d(' + (percent - 100) + '%,0,0)');
     }
 
     $active
         .carousel_transition(false)
-        .css('transform', 'translate3d(' + percent + '%, 0, 0)')
-  }
+        .css('transform', 'translate3d(' + percent + '%, 0, 0)');
+  };
 
   CarouselSwipe.prototype.getActive = function() {
-    this.$active = this.$element.find('.item.active')
-    this.$items = this.$active.parent().children()
+    this.$active = this.$element.find('.item.active');
+    this.$items = this.$active.parent().children();
 
-    this.$next = this.$active.next()
+    this.$next = this.$active.next();
     if (!this.$next.length && this.options.wrap) {
       this.$next = this.$items.first();
     }
 
-    this.$prev = this.$active.prev()
+    this.$prev = this.$active.prev();
     if (!this.$prev.length && this.options.wrap) {
       this.$prev = this.$items.last();
     }
 
     return this.$active;
-  }
+  };
 
   // CAROUSEL PLUGIN DEFINITION
   // ==========================
 
-  var old = $.fn.carousel
+  var old = $.fn.carousel;
   $.fn.carousel = function() {
     old.apply(this, arguments);
     return this.each(function () {
-      var $this   = $(this)
-      var data    = $this.data('bs.carousel.swipe')
+      var $this   = $(this);
+      var data    = $this.data('bs.carousel.swipe');
       if (!data) $this.data('bs.carousel.swipe', new CarouselSwipe(this))
     })
-  }
+  };
 
   $.extend($.fn.carousel,old);
 
@@ -163,7 +163,7 @@
     return this.each(function() {
       $(this)
         .css('-webkit-transition', enable)
-        .css('transition', enable)
+        .css('transition', enable);
     })
   };
 
