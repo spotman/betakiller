3dc11ddc4274f075faab493a4ca3551f40f8ed69
Spotman <i.am@spotman.ru>
2016-11-16 21:28:21 +0700

    remove unused modules

--- application/config/environments/development/modules.php
+++ application/config/environments/development/modules.php
@@ -1,10 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 return array(
-    'deployer'      => MODPATH.'deployer',      // Automatic deployment tool web UI
+//    'deployer'      => MODPATH.'deployer',      // Automatic deployment tool web UI
 //    'codebench'     => MODPATH.'codebench',     // Benchmarking tool
-    'unittest'      => MODPATH.'unittest',      // Unit testing
-    'userguide'     => MODPATH.'userguide',     // User guide and API documentation
-    'logviewer'     => MODPATH.'logviewer',     // Display logs
-    'console'       => MODPATH.'console',       // Interactive console
+//    'unittest'      => MODPATH.'unittest',      // Unit testing
+//    'userguide'     => MODPATH.'userguide',     // User guide and API documentation
+//    'logviewer'     => MODPATH.'logviewer',     // Display logs
+//    'console'       => MODPATH.'console',       // Interactive console
 );
