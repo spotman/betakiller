687e049c9a549f939b9127de2b4415eeb0fc12d0
Spotman <i.am@spotman.ru>
2016-11-19 16:33:01 +0700

    Add functions for checking table and column existence

--- application/classes/Migration.php
+++ application/classes/Migration.php
@@ -8,4 +8,25 @@ abstract class Migration extends Kohana_Migration
     {
         DB::query($type, $sql)->execute($db);
     }
+
+    protected function table_exists($table_name, $db = NULL)
+    {
+        return $this->table_has_column($table_name, NULL, $db);
+    }
+
+    protected function table_has_column($table_name, $column_name, $db = NULL)
+    {
+        try
+        {
+            DB::select($column_name)->from($table_name)->limit(1)->execute($db)->as_array();
+
+            // Query completed => table and column exists
+            return TRUE;
+
+        } catch (Database_Exception $e)
+        {
+            // Query failed => table or column is absent
+            return FALSE;
+        }
+    }
 }
