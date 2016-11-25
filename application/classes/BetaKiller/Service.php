be3a1e34f21540112949dd7f0cfb71f08e906471
Spotman <i.am@spotman.ru>
2016-11-25 10:52:10 +0700

    implement DI for Service::instance()

--- application/classes/BetaKiller/Service.php
+++ application/classes/BetaKiller/Service.php
@@ -1,10 +1,15 @@
 <?php
 namespace BetaKiller;
 
+use BetaKiller\DI\Container;
 use BetaKiller\Helper\Base;
 
 abstract class Service
 {
-    use Utils\Instance\Simple;
     use Base;
+
+    public static function instance()
+    {
+        return Container::instance()->get(static::class);
+    }
 }
