aaf4578da6dcf0d0cc9765cd4e050ce1f4b98025
Spotman <i.am@spotman.ru>
2016-12-28 21:38:23 +0800

    i18n message

--- modules/iface/i18n/ru.php
+++ modules/iface/i18n/ru.php
@@ -1,5 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 return array (
+  'Can not find :name model in parameters' => 'Can not find :name model in parameters',
   'Can not find [:prototype] item by [:value]' => 'Can not find [:prototype] item by [:value]',
 );
