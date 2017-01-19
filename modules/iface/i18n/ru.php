5082a5da4112652a735f9016d6fd965f2943f6b1
Spotman <i.am@spotman.ru>
2017-01-19 21:58:34 +0700

    iface i18n

--- modules/iface/i18n/ru.php
+++ modules/iface/i18n/ru.php
@@ -1,6 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 return array (
+  'No iface class found for :name' => 'No iface class found for :name',
   'Can not find :name model in parameters' => 'Can not find :name model in parameters',
   'Can not find [:prototype] item by [:value]' => 'Can not find [:prototype] item by [:value]',
 );
