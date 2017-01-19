96c2e6a91efe84bb0815623eeba0e45f41650e7d
Spotman <i.am@spotman.ru>
2017-01-19 21:57:37 +0700

    admin module (layout + admin bar + materializecss and bower.json)

---
+++ modules/admin/classes/Widget/Admin/Bar.php
@@ -0,0 +1,25 @@
+<?php
+
+class Widget_Admin_Bar extends \BetaKiller\IFace\Widget
+{
+    use \BetaKiller\Helper\CurrentUser;
+
+    /**
+     * Returns data for View rendering
+     *
+     * @return array
+     */
+    public function get_data()
+    {
+        $user = $this->current_user(true);
+
+        // If user is not authorized, then silently exiting
+        if (!$user || !$user->is_admin_allowed()) {
+            return [];
+        }
+
+        return [
+            'enabled' =>  true,
+        ];
+    }
+}
