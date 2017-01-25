c269701f767fa5945d4ba0f1f1e34d06161873ab
Spotman <i.am@spotman.ru>
2017-01-25 15:32:11 +0700

    cleanup unused code

--- application/classes/Task/Auth/PasswordHash.php
+++ application/classes/Task/Auth/PasswordHash.php
@@ -1,18 +1,13 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
-
-class Task_Auth_PasswordHash extends Minion_Task {
-
+class Task_Auth_PasswordHash extends Minion_Task
+{
     protected function _execute(array $params)
     {
-//        $password   = Minion_CLI::read("Enter the password");
-//        $confirm    = Minion_CLI::read("Enter the password again");
-
         $password   = $this->password("Enter the password");
         $confirm    = $this->password("Enter the password again");
 
-        if ( $password != $confirm )
-        {
+        if ($password != $confirm) {
             $this->write('Passwords are not identical', self::RED);
             return;
         }
@@ -21,5 +16,4 @@ class Task_Auth_PasswordHash extends Minion_Task {
 
         $this->write($hash, self::GREEN);
     }
-
 }
