013453fe9893973530a3be49cc65863d4cbfc900
Spotman <i.am@spotman.ru>
2017-05-03 21:14:08 +0300

    cron refactoring (crunz and Sitemap task are moved to core + unified cron executing logic)

---
+++ tasks/Sitemap.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------------------
+|  Task File
+|--------------------------------------------------------------------------------------
+|
+| This file basically registers a new task to be executed by Crunz
+| To get the list of all frequency and constraint method, you may
+| go to this link: https://github.com/lavary/crunz#scheduling-frequency-and-constraints
+|
+*/
+
+use Crunz\Schedule;
+
+$scheduler = new Schedule();
+
+$scheduler
+    ->run('php index.php --stage=production sitemap')
+    ->description('Sitemap generation')
+    ->in('public')
+    ->every('minutes', 5)
+    ->preventOverlapping();
+
+return $scheduler;
