abe881ea7f7accc5d553dc4a2154a5fbfd507226
Spotman <i.am@spotman.ru>
2017-05-31 21:14:25 +0300

    kohana php7 fix

--- system/utf8/ucwords.php
+++ system/utf8/ucwords.php
@@ -10,14 +10,17 @@
  */
 function _ucwords($str)
 {
-	if (UTF8::is_ascii($str))
-		return ucwords($str);
+	if (UTF8::is_ascii($str)) {
+        return ucwords($str);
+    }
 
-	// [\x0c\x09\x0b\x0a\x0d\x20] matches form feeds, horizontal tabs, vertical tabs, linefeeds and carriage returns.
-	// This corresponds to the definition of a 'word' defined at http://php.net/ucwords
-	return preg_replace(
-		'/(?<=^|[\x0c\x09\x0b\x0a\x0d\x20])[^\x0c\x09\x0b\x0a\x0d\x20]/ue',
-		'UTF8::strtoupper(\'$0\')',
-		$str
-	);
+    // [\x0c\x09\x0b\x0a\x0d\x20] matches form feeds, horizontal tabs, vertical tabs, linefeeds and carriage returns.
+    // This corresponds to the definition of a 'word' defined at http://php.net/ucwords
+    return preg_replace_callback(
+        '/(?<=^|[\x0c\x09\x0b\x0a\x0d\x20])[^\x0c\x09\x0b\x0a\x0d\x20]/u',
+        function($m) {
+            return UTF8::strtoupper($m[0]);
+        },
+        $str
+    );
 }
