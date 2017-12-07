83ee4b53951adf95c19bd6f20b2f2ee18d0e92fd
Spotman <i.am@spotman.ru>
2017-12-07 21:20:53 +0700

    better crontab

--- modules/cron/classes/Task/Cron.php
+++ modules/cron/classes/Task/Cron.php
@@ -20,12 +20,31 @@ class Task_Cron extends AbstractTask
     private $env;
 
     /**
+     * @todo Refactoring to database
+     * @var array
+     */
+    private $queue = [];
+
+    /**
      * @param array $params
      *
      * @throws \BetaKiller\Task\TaskException
      * @throws \Symfony\Component\Yaml\Exception\ParseException
      */
     protected function _execute(array $params): void
+    {
+        // Get all due tasks and enqueue them (long-running task would not affect next tasks due check)
+        $this->enqueueDueTasks();
+
+        // Get all queued tasks and run them one by one
+        $this->runQueuedTasks();
+    }
+
+    /**
+     * @throws \BetaKiller\Task\TaskException
+     * @throws \Symfony\Component\Yaml\Exception\ParseException
+     */
+    private function enqueueDueTasks(): void
     {
         $sitePath = $this->multiSite->getSitePath();
 
@@ -38,6 +57,8 @@ class Task_Cron extends AbstractTask
             throw new TaskException('Missing crontab.yml');
         }
 
+        $currentStage = $this->env->getModeName();
+
         foreach ($records as $name => $data) {
             $expr = $data['at'] ?? null;
 
@@ -45,38 +66,53 @@ class Task_Cron extends AbstractTask
                 throw new TaskException('Missing "at" key value in [:name] task', [':name' => $name]);
             }
 
-            $this->logger->debug('Checking is due :task', [':task' => $name]);
+            // Add stage if not defined
+            if (!isset($data['stage'])) {
+                $data['stage'] = $currentStage;
+            }
+
+            // Ensure that target stage is reached
+            if ($data['stage'] !== $currentStage) {
+                $this->logger->debug('Skipping task [:name] for stage :stage', [
+                    ':name'  => $name,
+                    ':stage' => $data['stage'],
+                ]);
+
+                continue;
+            }
 
             $cron = CronExpression::factory($expr);
 
-            if ($cron->isDue()) {
-                // TODO Enqueue task if not queued
-                $this->runTask($name, $data);
+            if (!$cron->isDue()) {
+                $this->logger->debug('Task [:task] is not due, skipping', [':task' => $name]);
+                continue;
+            }
+
+            $this->logger->debug('Task [:task] is due', [':task' => $name]);
+
+            // Enqueue task if not queued
+            if (!isset($this->queue[$name])) {
+                $this->queue[$name] = $data;
+                $this->logger->debug('Task [:task] was queued', [':task' => $name]);
             }
         }
+    }
 
-        // TODO Get first queued task and run it
+    private function runQueuedTasks(): void
+    {
+        if (!$this->queue) {
+            $this->logger->debug('No queued tasks, exiting');
+            return;
+        }
+
+        // Get every queued task and run it
+        foreach ($this->queue as $name => $data) {
+            $this->runTask($name, $data);
+        }
     }
 
     private function runTask(string $name, array $params)
     {
-        $stage = $this->env->getModeName();
-
-        // Add stage if not defined
-        if (!isset($params['stage'])) {
-            $params['stage'] = $stage;
-        }
-
-        // Ensure that target stage is reached
-        if ($params['stage'] !== $stage) {
-            $this->logger->debug('Skipping task :name for stage :stage', [
-                ':name'  => $name,
-                ':stage' => $params['stage'],
-            ]);
-
-            return;
-        }
-
         $sitePath  = $this->multiSite->getSitePath();
         $indexPath = $sitePath.DIRECTORY_SEPARATOR.'public';
 
