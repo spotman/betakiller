1f6219d96c08b94266e5720e6b73001e95ca950f
Spotman <i.am@spotman.ru>
2018-01-07 16:12:29 +0800

    fix OrmExtendedInterface

--- application/classes/BetaKiller/Model/ExtendedOrmInterface.php
+++ application/classes/BetaKiller/Model/ExtendedOrmInterface.php
@@ -1,12 +1,7 @@
 <?php
 namespace BetaKiller\Model;
 
-use BetaKiller\Search\ApplicableSearchModelInterface;
-use BetaKiller\Search\SearchResultsItemInterface;
 use BetaKiller\Utils\Kohana\ORM\OrmInterface;
 use Spotman\Api\ApiResponseItemInterface;
 
-interface ExtendedOrmInterface
-    extends OrmInterface, ApiResponseItemInterface, DispatchableEntityInterface, ApplicableSearchModelInterface, SearchResultsItemInterface
-{
-}
+interface ExtendedOrmInterface extends OrmInterface, ApiResponseItemInterface, DispatchableEntityInterface {}
