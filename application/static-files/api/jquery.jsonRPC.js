259aa083d8307cf34b8b19d3f58bd181ece27c32
Spotman <i.am@spotman.ru>
2018-02-08 17:30:24 +0800

    Fix JS API after migrating to jQuery 3.3.1

--- application/static-files/api/jquery.jsonRPC.js
+++ application/static-files/api/jquery.jsonRPC.js
@@ -10,104 +10,103 @@
  */
 
 (function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+  } else {
+    // Browser globals
+    factory(jQuery);
+  }
 }(function ($) {
 
-    var namedHash = function(namedParams)
-    {
-        var params = namedParams;
+  var namedHash = function (namedParams) {
+    var params = namedParams;
 
-        this.getParams = function()
-        {
-            return params;
-        };
+    this.getParams = function () {
+      return params;
+    };
+  };
+
+  /**
+   * Thanks to JSON-RPC Tool
+   * c. 2012 CJ Holmes - Free for all use provided attribution is given.
+   * @link http://github.com/slashingweapon/jsonTools
+   * @param url string
+   * @param resource string
+   * @param method string
+   * @returns {ajax}
+   */
+  $.fn.jsonRPC = function (url, resource, method) {
+    var JSONRPCError = function (obj) {
+      $.extend(this, obj);
+      this.toString = function () {
+        return this.message;
+      };
+      this.getCode = function () {
+        return this.code;
+      };
     };
 
-    /**
-     * Thanks to JSON-RPC Tool
-     * c. 2012 CJ Holmes - Free for all use provided attribution is given.
-     * @link http://github.com/slashingweapon/jsonTools
-     * @param url string
-     * @param resource string
-     * @param method string
-     * @returns {ajax}
-     */
-    $.fn.jsonRPC = function(url, resource, method)
-    {
-        var JSONRPCError = function (obj) {
-            jQuery.extend(this, obj);
-            this.toString = function() { return this.message; };
-            this.getCode = function() { return this.code; };
-        };
+    // Keep a static counter, so we can give a unique ID to each request.  This technique
+    // even survives a function name change.
+    arguments.callee.requestCount = arguments.callee.requestCount || 0;
+    arguments.callee.requestCount++;
 
-        // Keep a static counter, so we can give a unique ID to each request.  This technique
-        // even survives a function name change.
-        arguments.callee.requestCount = arguments.callee.requestCount || 0;
-        arguments.callee.requestCount++;
+    var params = [];
 
-        var params = [];
+    // Take the extra parameters and push them onto the params array.
+    for (var idx = 3; idx < arguments.length; idx++) {
+      params.push(arguments[idx]);
+    }
 
-        // Take the extra parameters and push them onto the params array.
-        for (var idx=3; idx<arguments.length; idx++)
-            params.push(arguments[idx]);
-
-        var request = {
-            jsonrpc: "2.0",
-            id: arguments.callee.requestCount,
-            method: resource + "." + method
-        };
-
-        if ( params.length > 0 )
-        {
-            // If first parameter is object, then we use it as hash with named parameters
-            request.params = (params[0] instanceof namedHash)
-                ? params[0].getParams()
-                : params;
-        }
-
-        if ( params && params.length )
-        {
-            request.params = params;
-        }
-
-        var jqXHR = jQuery.ajax({
-            type:'post',
-            url: url,
-            contentType: 'application/json',
-            data: JSON.stringify(request),
-            dataType: 'json_rpc_response',
-            converters: {
-                "text json_rpc_response": function(textValue) {
-                    var retval = jQuery.parseJSON(textValue);
-
-                    if (typeof(retval) === 'object') {
-                        if (typeof(retval.error) === 'object') {
-                            retval = new JSONRPCError(retval.error);
-                            throw retval;
-                        } else if (typeof(retval.result) !== 'undefined') {
-                            retval = retval.result;
-                        } else
-                            throw "Invalid JSON response";
-                    } else
-                        throw "Invalid JSON response";
-
-                    return retval;
-                }
-            }
-        });
-
-        jqXHR.jsonRequestObject = request;
-
-        return jqXHR;
+    var request = {
+      jsonrpc: "2.0",
+      id: arguments.callee.requestCount,
+      method: resource + "." + method
     };
 
-    $.fn.jsonRPC.named = namedHash;
+    if (params.length > 0) {
+      // If first parameter is object, then we use it as hash with named parameters
+      request.params = (params[0] instanceof namedHash)
+        ? params[0].getParams()
+        : params;
+    }
 
-    return $.fn.jsonRPC;
+    if (params && params.length) {
+      request.params = params;
+    }
+
+    var jqXHR = $.ajax({
+      type: 'post',
+      url: url,
+      contentType: 'application/json',
+      data: JSON.stringify(request),
+      dataType: 'json_rpc_response',
+      converters: {
+        "text json_rpc_response": function (textValue) {
+          var retval = $.parseJSON(textValue);
+
+          if (typeof(retval) !== 'object') {
+            throw "Invalid JSON response";
+          }
+
+          if (typeof(retval.error) === 'object') {
+            throw new JSONRPCError(retval.error);
+          } else if (typeof(retval.result) !== 'undefined') {
+            return retval.result;
+          } else {
+            throw "Invalid JSON response";
+          }
+        }
+      }
+    });
+
+    jqXHR.jsonRequestObject = request;
+
+    return jqXHR;
+  };
+
+  $.fn.jsonRPC.named = namedHash;
+
+  return $.fn.jsonRPC;
 }));
--- application/static-files/api/rpc.factory.js
+++ application/static-files/api/rpc.factory.js
@@ -3,167 +3,170 @@
  * See more at https://github.com/spotman/kohana-simple-api
  */
 define([
-    "jquery",
-    "jquery.jsonRPC",
-    "auth.modal"
-], function($, jsonRPC, authModal) {
+  "jquery",
+  "jquery.jsonRPC",
+  "auth.modal"
+], function ($, jsonRPC, authModal) {
 
-    return function(url) {
+  return function (url) {
 
-        url = url || "/api/v1/json-rpc";
+    url = url || "/api/v1/json-rpc";
 
-        var authLock = false,
-            requestQueue = [];
+    var authLock     = false,
+        requestQueue = [];
 
-        /**
-         * @param args Array
-         * @returns {Deferred}
-         */
-        function addToQueue(args) {
+    /**
+     * @param args Array
+     * @param deferred {Deferred}
+     */
+    function addToQueue(args, deferred) {
+      requestQueue.push({
+        args: args,
+        deferred: deferred
+      });
+    }
 
-            var deferred = $.Deferred();
+    function resolveQueue() {
+      processQueue("resolve");
+    }
 
-            requestQueue.push({
-                args: args,
-                deferred: deferred
-            });
+    function rejectQueue() {
+      processQueue("reject");
+    }
 
-//            console.log(args[1] + "." + args[2] + " is queued");
+    function processQueue(method) {
+      //console.log("processing queue with [" + method + "] method");
+      //console.log(requestQueue);
 
-            return deferred;
+      // Walk through queue and resolve each deferred
+      while (requestQueue.length) {
+        (function (queuedRequest) {
+          var queuedArgs     = queuedRequest.args,
+              queuedDeferred = queuedRequest.deferred;
+
+          //console.log(method + " queued " + queuedArgs[0] + "." + queuedArgs[0]);
+
+          if (method === "resolve") {
+            doRequest(queuedArgs)
+              .done(function () {
+                //console.log(method + " queued " + queuedArgs[0] + "." + queuedArgs[0] + " done!");
+                queuedDeferred.resolveWith({}, arguments);
+              })
+              .fail(function () {
+                //console.log(method + " queued " + queuedArgs[0] + "." + queuedArgs[0] + " failed!");
+                queuedDeferred.rejectWith({}, arguments);
+              });
+          } else if (method === "reject") {
+            queuedDeferred.reject();
+          } else {
+            throw new Error("Unknown method " + method);
+          }
+
+        }(requestQueue.shift()));
+      }
+
+      //console.log("queue processed");
+      //console.log("queue length = " + requestQueue.length);
+    }
+
+    function clearQueue() {
+      requestQueue = [];
+      //console.log("queue cleared");
+    }
+
+    function lock() {
+      authLock = true;
+      //console.log("API locked");
+    }
+
+    function unlock() {
+      authLock = false;
+      //console.log("API unlocked");
+    }
+
+    function doRequest(requestArguments) {
+      var deferred = $.Deferred();
+
+      // Queue request when api is locked
+      if (authLock) {
+        addToQueue(arguments, deferred);
+
+        return deferred;
+      }
+
+      var xhr = jsonRPC.apply({}, requestArguments);
+
+      // Proxy successful call
+      xhr.done(function () {
+        deferred.resolveWith({}, arguments);
+      });
+
+      // Proxy failed call and wrap it if auth requested
+      xhr.fail(function (xhr, name, JSONRPCError) {
+        var code = JSONRPCError.getCode ? JSONRPCError.getCode() : null;
+
+        if (code === 401) {
+          // Lock future API requests
+          lock();
+
+          // Queue current request
+          addToQueue(requestArguments, deferred);
+
+          // Show auth modal form
+          authRequiredModal();
+
+          return;
         }
 
-        function resolveQueue() {
-            processQueue("resolve");
-        }
+        var message = JSONRPCError ? JSONRPCError.toString() : null;
 
-        function rejectQueue() {
-            processQueue("reject");
-        }
+        deferred.rejectWith({}, [message]);
+      });
 
-        function processQueue(method) {
+      return deferred;
+    }
 
-//            console.log("processing queue with [" + method + "] method");
-//            console.log(requestQueue);
+    function authRequiredModal() {
+      var successCallback = function () {
+        // Unlock API
+        unlock();
 
-            // Walk through queue and resolve each deferred
-            while (requestQueue.length) {
-              (function(queuedRequest) {
-                var queuedArgs = queuedRequest.args,
-                    queuedDeferred = queuedRequest.deferred;
+        // Process all queued requests
+        resolveQueue();
 
-                // console.log(method + " queued " + queuedArgs[0] + "." + queuedArgs[0]);
+        // Cleanup queue
+        clearQueue();
+      };
 
-                if (method === "resolve") {
-                  doRequest(queuedArgs)
-                    .done(function() {
-//                            console.log(method + " queued " + queuedArgs[0] + "." + queuedArgs[0] + " done!");
-                      queuedDeferred.resolveWith({}, arguments);
-                    })
-                    .fail(function() {
-//                            console.log(method + " queued " + queuedArgs[0] + "." + queuedArgs[0] + " failed!");
-                      queuedDeferred.rejectWith({}, arguments);
-                    });
-                } else if (method === "reject") {
-                  queuedDeferred.reject();
-                } else {
-                  throw new Error("Unknown method " + method);
-                }
+      var errorCallback = function () {
+        // Reject all queued request
+        rejectQueue();
 
-              }(requestQueue.shift()));
-            }
+        // Cleanup queue
+        clearQueue();
 
-//            console.log("queue processed");
-//            console.log("queue length = " + requestQueue.length);
-        }
+        // Allow repeat of the auth
+        unlock();
+      };
 
-        function clearQueue() {
-            requestQueue = [];
-//            console.log("queue cleared");
-        }
+      // Show modal and provide callbacks
+      authModal.show(successCallback, errorCallback);
+    }
 
-        function lock() {
-            authLock = true;
-//            console.log("API locked");
-        }
+    return function (service, method, proxyArguments) {
+      var requestArguments = [url, service, method];
 
-        function unlock() {
-            authLock = false;
-//            console.log("API unlocked");
-        }
+      proxyArguments = proxyArguments || [];
 
-        function doRequest(requestArguments) {
+      for (var idx = 0; idx < proxyArguments.length; idx++) {
+        requestArguments.push(proxyArguments[idx]);
+      }
 
-            // Queue request when api is locked
-            if ( authLock )
-                return addToQueue(arguments);
-
-            return jsonRPC.apply({}, requestArguments)
-
-                // Overwrite original Deferred with queue callback
-                .then(null, function(xhr, name, JSONRPCError) {
-                    var code = JSONRPCError.getCode ? JSONRPCError.getCode() : null;
-
-                    if (code === 401) {
-                        // Lock future API requests
-                        lock();
-
-                        // Queue current request and return new JQuery.Deferred
-                        var newPromise = addToQueue(requestArguments);
-
-                        // Show auth modal form
-                        authRequiredModal();
-
-                        return newPromise;
-                    }
-
-                    return JSONRPCError;
-                });
-        }
-
-        function authRequiredModal() {
-
-            var successCallback = function() {
-                // Unlock API
-                unlock();
-
-                // Process all queued requests
-                resolveQueue();
-
-                // Cleanup queue
-                clearQueue();
-            };
-
-            var errorCallback = function() {
-
-                // Reject all queued request
-                rejectQueue();
-
-                // Cleanup queue
-                clearQueue();
-
-                // Allow repeat of the auth
-                unlock();
-            };
-
-            // Show modal and provide callbacks
-            authModal.show(successCallback, errorCallback);
-        }
-
-        return function (service, method, proxyArguments) {
-
-            var requestArguments = [url, service, method];
-
-            proxyArguments = proxyArguments || [];
-
-            for (var idx = 0; idx < proxyArguments.length; idx++)
-                requestArguments.push(proxyArguments[idx]);
-
-            /**
-             * @link http://api.jquery.com/deferred.then/
-             */
-            return doRequest(requestArguments);
-        }
-    };
+      /**
+       * @link http://api.jquery.com/deferred.then/
+       */
+      return doRequest(requestArguments);
+    }
+  };
 
 });
