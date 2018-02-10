cd030673f21e870d50297d33c79fc948040dc7b5
Spotman <i.am@spotman.ru>
2018-02-10 09:23:38 +0800

    Replace HTTP_Exception_302 exception with FoundHttpException in ResponseHelper

--- application/classes/BetaKiller/Helper/ResponseHelper.php
+++ application/classes/BetaKiller/Helper/ResponseHelper.php
@@ -2,14 +2,17 @@
 namespace BetaKiller\Helper;
 
 
+use BetaKiller\Exception\FoundHttpException;
+
 class ResponseHelper
 {
     /**
      * @param string $url
-     * @throws \HTTP_Exception_302
+     *
+     * @throws \BetaKiller\Exception\FoundHttpException
      */
     public function redirect(string $url): void
     {
-        \HTTP::redirect($url);
+        throw new FoundHttpException($url);
     }
 }
