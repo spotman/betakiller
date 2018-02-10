be39070d4e64bf22be40fefe09edd4f34a390f6b
Spotman <i.am@spotman.ru>
2018-02-10 20:44:18 +0800

    Add check for required option in AbstractTask::getOption() method

--- application/classes/BetaKiller/Task/AbstractTask.php
+++ application/classes/BetaKiller/Task/AbstractTask.php
@@ -5,8 +5,22 @@ abstract class AbstractTask extends \Minion_Task
 {
     public const CLI_USER_NAME = 'minion';
 
-    protected function getOption(string $key): ?string
+    /**
+     * @param string    $key
+     * @param bool|null $required
+     *
+     * @return null|string
+     * @throws \BetaKiller\Task\TaskException
+     */
+    protected function getOption(string $key, ?bool $required = null): ?string
     {
-        return $this->_options[$key] ?? null;
+        $required = $required ?? true;
+        $value = $this->_options[$key] ?? null;
+
+        if ($value === null && $required) {
+            throw new TaskException('Option :name is required', [':name' => $key]);
+        }
+
+        return $value;
     }
 }
