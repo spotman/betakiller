a1d8f4ae992632a7b83e95f45548ab23657ae9f1
Spotman <i.am@spotman.ru>
2018-02-21 12:20:24 +0800

    Utils submodule + security-headers tweak

--- modules/security-headers/init.php
+++ modules/security-headers/init.php
@@ -1,16 +1,16 @@
 <?php
 
-$base_url = Kohana::$base_url;
+$baseUrl = Kohana::$base_url;
 
 $headers = new SecureHeaders();
 $headers->hsts();
 $headers->csp('default', 'self');
 
-$headers->csp('style', 'unsafe-inline');
-$headers->csp('style', $base_url);
+//$headers->csp('style', 'unsafe-inline');
+$headers->csp('style', $baseUrl);
 
-$headers->csp('script', $base_url);
-$headers->csp('script', 'unsafe-inline');
+$headers->csp('script', $baseUrl);
+//$headers->csp('script', 'unsafe-inline');
 
 $headers->safeMode();
 $headers->doneOnOutput();
