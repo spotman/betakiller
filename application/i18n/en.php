3918ac88b4a761b86a6290474c07206790e6c83e
Spotman <i.am@spotman.ru>
2018-06-27 16:04:57 +0300

    Add default translations for en language

--- application/i18n/en.php
+++ application/i18n/en.php
@@ -9,4 +9,12 @@ return [
     'error.auth.incorrect_password' => 'Your credentials are invalid',
     'error.auth.user_not_exists'    => 'User does not exists',
     'error.auth.wrong_ip'           => 'Your IP address is not valid',
+
+    'No class found for :name, tried to autoload :tried' => 'No class found for :name, tried to autoload :tried',
+    'arguments' => 'arguments',
+    'PHP internal call' => 'PHP internal call',
+    'Environment' => 'Environment',
+    'Included files' => 'Included files',
+    'Loaded extensions' => 'Loaded extensions',
+    ':error [ :query ]' => ':error [ :query ]',
 ];
