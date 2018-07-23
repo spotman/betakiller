e11a99ed116f0d5fe15a8cb0d4ce2d46afcd0f9f
Spotman <i.am@spotman.ru>
2018-07-23 16:11:33 +0300

    I18n update

--- modules/platform/i18n/en.php
+++ modules/platform/i18n/en.php
@@ -10,4 +10,9 @@ return [
     'error.auth.user_not_exists'    => 'User does not exists',
     'error.auth.wrong_ip'           => 'Your IP address is not valid',
     'error.auth.denied'             => 'Access denied',
+
+    'Your username or email' => 'Your username or email',
+    'Your password' => 'Your password',
+    'Remember me' => 'Remember me',
+    'Sign in' => 'Sign in',
 ];
