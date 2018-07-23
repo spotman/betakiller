ab9acd91826c851a55a33bd25505a1ce2948a9e3
Spotman <i.am@spotman.ru>
2018-07-23 16:11:33 +0300

    I18n update

---
+++ modules/iface/i18n/en.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'UrlElement :name must have linked entity or custom ACL rules to protect itself' => 'UrlElement :name must have linked entity or custom ACL rules to protect itself',
+];
