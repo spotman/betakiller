73fef3a19cb1bab0b26fc56a0657e5004735e5b3
psa <psaproxy@gmail.com>
2018-08-06 12:32:12 +0300

    update adding filters for items #18

--- modules/iface/classes/BetaKiller/Url/UrlElementTreeLayerIterator.php
+++ modules/iface/classes/BetaKiller/Url/UrlElementTreeLayerIterator.php
@@ -1,32 +1,37 @@
 <?php
 namespace BetaKiller\Url;
 
-class UrlElementTreeLayerIterator extends \ArrayIterator
+class UrlElementTreeLayerIterator extends \FilterIterator
 {
+    /**
+     * IFace URL element filters
+     *
+     * @var \BetaKiller\Url\UrlElementFilterInterface
+     */
+    private $filters;
+
     /**
      * UrlElementTreeLayerIterator constructor.
      *
-     * @param \BetaKiller\Url\UrlElementTreeInterface $tree
-     *
-     * @param \BetaKiller\Url\UrlElementInterface     $parent
+     * @param \BetaKiller\Url\UrlElementTreeInterface        $tree
+     * @param \BetaKiller\Url\UrlElementInterface            $parent
+     * @param \BetaKiller\Url\UrlElementFilterInterface|null $filters [optional]
      *
      * @throws \BetaKiller\IFace\Exception\IFaceException
      */
-    public function __construct(UrlElementTreeInterface $tree, UrlElementInterface $parent = null)
-    {
+    public function __construct(
+        UrlElementTreeInterface $tree,
+        ?UrlElementInterface $parent = null,
+        ?UrlElementFilterInterface $filters = null
+    ) {
+        $this->filters = $filters;
+
         $layer = $parent
             ? $tree->getChildren($parent)
             : $tree->getRoot();
 
-        parent::__construct($layer, \ArrayObject::STD_PROP_LIST);
-    }
-
-    /**
-     * @return \BetaKiller\Url\UrlElementInterface[]
-     */
-    public function getArrayCopy(): array
-    {
-        return parent::getArrayCopy();
+        $layer = new \ArrayIterator($layer);
+        parent::__construct($layer);
     }
 
     /**
@@ -35,5 +40,21 @@ class UrlElementTreeLayerIterator extends \ArrayIterator
     public function current(): UrlElementInterface
     {
         return parent::current();
+    }
+
+    /**
+     * Checking availability IFace URL element by filters
+     *
+     * @return bool
+     */
+    public function accept(): bool
+    {
+        if ($this->filters) {
+            $urlElement = $this->current();
+
+            return $this->filters->isAvailable($urlElement);
+        }
+
+        return true;
     }
 }
