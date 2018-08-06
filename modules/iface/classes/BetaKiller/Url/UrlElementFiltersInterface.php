d9c8662905a3bce79670e41d0b3f353ed77c8e0c
psa <psaproxy@gmail.com>
2018-08-06 12:30:19 +0300

    add base filter for IFace URLs element #18

---
+++ modules/iface/classes/BetaKiller/Url/UrlElementFiltersInterface.php
@@ -0,0 +1,26 @@
+<?php
+namespace BetaKiller\Url;
+
+/**
+ * Collector of IFace URL element filters
+ */
+interface UrlElementFiltersInterface extends UrlElementFilterInterface
+{
+    /**
+     * Adding a filter
+     *
+     * @param \BetaKiller\Url\UrlElementFilterInterface $urlFilter
+     *
+     * @return $this
+     */
+    public function addFilter(UrlElementFilterInterface $urlFilter): self;
+
+    /**
+     * Checking availability IFace URL element by all filters
+     *
+     * @param \BetaKiller\Url\UrlElementInterface $urlElement
+     *
+     * @return bool
+     */
+    public function isAvailable(UrlElementInterface $urlElement): bool;
+}
