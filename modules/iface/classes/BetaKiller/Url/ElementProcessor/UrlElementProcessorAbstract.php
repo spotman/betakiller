fd51d48ad74828536c47d504a3bec1be77ac6b0b
psa <psaproxy@gmail.com>
2018-08-07 18:56:39 +0300

    update arguments in method "process" #37

--- modules/iface/classes/BetaKiller/Url/ElementProcessor/UrlElementProcessorAbstract.php
+++ modules/iface/classes/BetaKiller/Url/ElementProcessor/UrlElementProcessorAbstract.php
@@ -11,13 +11,6 @@ abstract class UrlElementProcessorAbstract implements UrlElementProcessorInterfa
     protected $request;
 
     /**
-     * Response controller
-     *
-     * @var \Response
-     */
-    protected $response;
-
-    /**
      * Setting request controller
      *
      * @param \Request $request
@@ -27,20 +20,6 @@ abstract class UrlElementProcessorAbstract implements UrlElementProcessorInterfa
     public function setRequest(\Request $request): UrlElementProcessorInterface
     {
         $this->request = $request;
-
-        return $this;
-    }
-
-    /**
-     * Setting response controller
-     *
-     * @param \Response $response
-     *
-     * @return $this
-     */
-    public function setResponse(\Response $response): UrlElementProcessorInterface
-    {
-        $this->response = $response;
 
         return $this;
     }
--- modules/iface/classes/BetaKiller/Url/ElementProcessor/UrlElementProcessorInterface.php
+++ modules/iface/classes/BetaKiller/Url/ElementProcessor/UrlElementProcessorInterface.php
@@ -1,6 +1,9 @@
 <?php
 namespace BetaKiller\Url\ElementProcessor;
 
+use \BetaKiller\Url\UrlElementInterface;
+use \BetaKiller\Url\Container\UrlContainerInterface;
+
 /**
  * URL element processor like IFace, WabHok and etc
  */
@@ -8,20 +11,21 @@ interface UrlElementProcessorInterface
 {
     /**
      * Execute processing on URL element
+     *
+     * @param \BetaKiller\Url\UrlElementInterface                  $model
+     * @param \BetaKiller\Url\Container\UrlContainerInterface|null $urlContainer [optional]
+     * @param \Response|null                                       $response [optional]
      */
-    public function process(): void;
+    public function process(
+        UrlElementInterface $model,
+        ?UrlContainerInterface  $urlContainer = null,
+        ?\Response  $response = null
+    ): void;
 
     /**
      * @param \Request $request
      *
      * @return \BetaKiller\Url\ElementProcessor\UrlElementProcessorInterface
      */
-    public function setRequest(\Request $request): self;
-
-    /**
-     * @param \Response $response
-     *
-     * @return \BetaKiller\Url\ElementProcessor\UrlElementProcessorInterface
-     */
-    public function setResponse(\Response $response): self;
+    public function setRequest(\Request $request): UrlElementProcessorInterface;
 }
