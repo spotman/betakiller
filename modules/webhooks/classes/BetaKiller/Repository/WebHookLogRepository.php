f5ae28a323c5f3532142bba5cbbafd8665f5d262
psa <psaproxy@gmail.com>
2018-08-15 21:37:47 +0300

    add repository of webhook log #37

---
+++ modules/webhooks/classes/BetaKiller/Repository/WebHookLogRepository.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types=1);
+
+namespace BetaKiller\Repository;
+
+use BetaKiller\Model\ExtendedOrmInterface;
+
+class WebHookLogRepository extends AbstractOrmBasedRepository
+{
+    /**
+     * @param string $codeName
+     *
+     * @return \BetaKiller\Model\WebHookLog[]
+     * @throws \BetaKiller\Factory\FactoryException
+     * @throws \BetaKiller\Repository\RepositoryException
+     */
+    public function getItems(string $codeName): array
+    {
+        $orm = $this->getOrmInstance();
+
+        return $this
+            ->filterByCodename($orm, $codeName)
+            ->orderByCreatedAtDesc($orm)
+            ->findAll($orm);
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return array
+     * @throws \BetaKiller\Exception
+     * @throws \BetaKiller\Factory\FactoryException
+     * @throws \BetaKiller\Repository\RepositoryException
+     */
+    public function getItemRequestData(int $id): array
+    {
+        $orm = $this->getOrmInstance();
+
+        $data = $this
+            ->filterById($orm, $id)
+            ->findOne($orm);
+
+        $value = [];
+        if ($data) {
+            /**
+             * @var \BetaKiller\Model\WebHookLog $data
+             */
+            $value = $data->getRequestData();
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param \BetaKiller\Model\ExtendedOrmInterface $orm
+     * @param int                                    $id
+     *
+     * @return \BetaKiller\Repository\WebHookLogRepository
+     */
+    private function filterById(ExtendedOrmInterface $orm, int $id): self
+    {
+        $orm->where('id', '=', $id);
+
+        return $this;
+    }
+
+    /**
+     * @param \BetaKiller\Model\ExtendedOrmInterface $orm
+     * @param string                                 $codeName
+     *
+     * @return \BetaKiller\Repository\WebHookLogRepository
+     */
+    private function filterByCodename(ExtendedOrmInterface $orm, string $codeName): self
+    {
+        $orm->where('codename', '=', $codeName);
+
+        return $this;
+    }
+
+    /**
+     * @param \BetaKiller\Model\ExtendedOrmInterface $orm
+     *
+     * @return \BetaKiller\Repository\WebHookLogRepository
+     */
+    private function orderByCreatedAtDesc(ExtendedOrmInterface $orm): self
+    {
+        $orm->order_by($orm->object_column('created_at'), 'DESC');
+
+        return $this;
+    }
+}
