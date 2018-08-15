00a3c01e49e2bd1754603cd7cb5a9635371bb503
psa <psaproxy@gmail.com>
2018-08-15 21:40:22 +0300

    fix access of constant iface codename #37 add retrieving log data items #37

--- modules/webhooks/classes/BetaKiller/IFace/Admin/WebHooks/InfoItem.php
+++ modules/webhooks/classes/BetaKiller/IFace/Admin/WebHooks/InfoItem.php
@@ -5,6 +5,7 @@ namespace BetaKiller\IFace\Admin\WebHooks;
 
 use BetaKiller\Factory\WebHookFactory;
 use BetaKiller\IFace\Admin\AbstractAdminBase;
+use BetaKiller\Repository\WebHookLogRepository;
 use BetaKiller\Url\Container\UrlContainer;
 use BetaKiller\Url\Container\UrlContainerInterface;
 use BetaKiller\Url\UrlElementTreeInterface;
@@ -13,7 +14,7 @@ use BetaKiller\Helper\IFaceHelper;
 
 class InfoItem extends AbstractAdminBase
 {
-    const LIST_ITEMS_IFACE_CODENAME = 'Admin_WebHooks_ListItems';
+    private const LIST_ITEMS_IFACE_CODENAME = 'Admin_WebHooks_ListItems';
 
     /**
      * @var \BetaKiller\Url\Container\UrlContainerInterface
@@ -36,21 +37,29 @@ class InfoItem extends AbstractAdminBase
     private $tree;
 
     /**
+     * @var \BetaKiller\Repository\WebHookLogRepository
+     */
+    private $webHookLogRepository;
+
+    /**
      * @param \BetaKiller\Url\UrlElementTreeInterface         $tree
      * @param \BetaKiller\Url\Container\UrlContainerInterface $urlContainer
      * @param \BetaKiller\Factory\WebHookFactory              $webHookFactory
      * @param \BetaKiller\Helper\IFaceHelper                  $ifaceHelper
+     * @param \BetaKiller\Repository\WebHookLogRepository     $webHookLogRepository
      */
     public function __construct(
         UrlElementTreeInterface $tree,
         UrlContainerInterface $urlContainer,
         WebHookFactory $webHookFactory,
-        IFaceHelper $ifaceHelper
+        IFaceHelper $ifaceHelper,
+        WebHookLogRepository $webHookLogRepository
     ) {
-        $this->urlContainer   = $urlContainer;
-        $this->webHookFactory = $webHookFactory;
-        $this->ifaceHelper    = $ifaceHelper;
-        $this->tree           = $tree;
+        $this->urlContainer         = $urlContainer;
+        $this->webHookFactory       = $webHookFactory;
+        $this->ifaceHelper          = $ifaceHelper;
+        $this->tree                 = $tree;
+        $this->webHookLogRepository = $webHookLogRepository;
     }
 
     /**
@@ -60,17 +69,19 @@ class InfoItem extends AbstractAdminBase
      */
     public function getData(): array
     {
+        $model = $this->urlContainer->getEntity(WebHookModelInterface::URL_CONTAINER_KEY);
+
+        //
         $urlElement   = $this->tree->getByCodename(self::LIST_ITEMS_IFACE_CODENAME);
         $listItemsUrl = $this->ifaceHelper->makeUrl($urlElement, null, false);
 
-        $model   = $this->urlContainer->getEntity(WebHookModelInterface::URL_CONTAINER_KEY);
+        //
         $webHook = $this->webHookFactory->createFromUrlElement($model);
+        $request = $webHook->getRequestDefinition();
 
         $param = UrlContainer::create();
         $param->setEntity($model);
         $requestAction = $this->ifaceHelper->makeUrl($model, $param, false);
-
-        $request = $webHook->getRequestDefinition();
 
         $codeName    = $model->getCodename();
         $serviceName = $model->getServiceName();
@@ -81,6 +92,21 @@ class InfoItem extends AbstractAdminBase
             'event'   => $eventName,
         ];
 
+        //
+        $logItems = $this->webHookLogRepository->getItems($model->getCodename());
+        foreach ($logItems as &$logItem) {
+            $logItem = [
+                'id'          => $logItem->getID(),
+                'codeName'    => $logItem->getCodename(),
+                'dateCreated' => $logItem->getCreatedAt(),
+                'status'      => (int)$logItem->isStatusSucceeded(),
+                'message'     => $logItem->getMessage(),
+                'requestData' => $logItem->getRequestData(),
+            ];
+        }
+
+
+        //
         return [
             'listItemsUrl' => $listItemsUrl,
             'info'         => $info,
@@ -89,6 +115,7 @@ class InfoItem extends AbstractAdminBase
                 'method' => $request->getMethod(),
                 'fields' => $request->getFields(),
             ],
+            'logItems'     => $logItems,
         ];
     }
 }
