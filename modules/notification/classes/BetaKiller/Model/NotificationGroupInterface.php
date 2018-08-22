7cc268d3a4f4d94ef4f39684d58f10a47f4830b4
psa <psaproxy@gmail.com>
2018-08-22 15:57:31 +0300

    update renamed table name and removed unused logic #29

--- modules/notification/classes/BetaKiller/Model/NotificationGroupInterface.php
+++ modules/notification/classes/BetaKiller/Model/NotificationGroupInterface.php
@@ -4,11 +4,6 @@ namespace BetaKiller\Model;
 interface NotificationGroupInterface
 {
     /**
-     * @return array[string self::TABLE_FIELD_CODENAME, string self::TABLE_FIELD_DESCRIPTION]
-     */
-    public function getAll(): array;
-
-    /**
      * @return string
      */
     public function getCodename(): string;
