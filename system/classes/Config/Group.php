422739dc796123bb20adc1e70a9027cda30d4574
psa <psaproxy@gmail.com>
2018-08-31 12:41:02 +0300

    update removing wrapping method

--- system/classes/Config/Group.php
+++ system/classes/Config/Group.php
@@ -1,14 +1,4 @@
-<?php
-defined('SYSPATH') OR die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 
-class Config_Group extends Kohana_Config_Group
-{
-    /**
-     * @return array
-     */
-    public function asArray(): array
-    {
-        return $this->as_array();
-    }
-}
+class Config_Group extends Kohana_Config_Group {}
