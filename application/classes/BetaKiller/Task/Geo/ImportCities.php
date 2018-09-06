952d60dfd4bd9c3c6d790fdc38148ca058cae9e9
psa <psaproxy@gmail.com>
2018-09-06 18:05:01 +0300

    #57 working on task for geo countries & cities import

--- application/classes/BetaKiller/Task/Geo/ImportCities.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace BetaKiller\Task\Geo;
-
-class ImportCities extends Providers\ImportCitiesMaxMindProvider
-{
-}
--- application/classes/BetaKiller/Task/Geo/ImportCountries.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace BetaKiller\Task\Geo;
-
-class ImportCountries extends Providers\ImportCountriesMaxMindProvider
-{
-}
-
