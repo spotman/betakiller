581c48ee39c63f774982dc288110f7789d0a594f
Spotman <i.am@spotman.ru>
2018-09-10 19:59:39 +0300

    Introduce phpstan + fix QA issues + improve testing + drone config

--- modules/platform/classes/BetaKiller/Repository/LanguageRepository.php
+++ modules/platform/classes/BetaKiller/Repository/LanguageRepository.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace BetaKiller\Repository;
 
 /**
- * @method \BetaKiller\Model\AbstractEntityInterface[]|\Traversable getAll()
+ * @method \BetaKiller\Model\LanguageInterface[] getAll()
  */
 class LanguageRepository extends AbstractOrmBasedRepository implements LanguageRepositoryInterface
 {
