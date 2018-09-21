6ca69557c494339389e96c633e44128c9d30bc10
Spotman <i.am@spotman.ru>
2018-09-21 15:33:58 +0300

    Fix QA issues + remove .idea files

--- modules/platform/classes/BetaKiller/Api/ApiMethodException.php
+++ modules/platform/classes/BetaKiller/Api/ApiMethodException.php
@@ -1,11 +1,9 @@
 <?php
 declare(strict_types=1);
 
-namespace BetaKiller\Api\Method;
+namespace BetaKiller\Api;
 
-use BetaKiller\Exception;
-
-class ApiMethodException extends Exception
+class ApiMethodException extends \Spotman\Api\ApiMethodException
 {
     /**
      * @return bool
