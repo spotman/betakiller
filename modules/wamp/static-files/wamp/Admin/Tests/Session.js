21a76507a6e4dd61f28a508382dcbb657f4aa3a3
psa <psaproxy@gmail.com>
2018-10-01 16:58:45 +0300

    #74 working on WAMP production

--- modules/wamp/static-files/wamp/Admin/Tests/Session.js
+++ modules/wamp/static-files/wamp/Admin/Tests/Session.js
@@ -5,8 +5,20 @@ export class Session {
     this.cookieName      = cookieName
     this.cookieSeparator = cookieSeparator
   }
+
   getId() {
-    this.cookieName      = cookieName
-    this.cookieSeparator = cookieSeparator
+    let id = super._readCookie(this.cookieName)
+    if (this.cookieSeparator) {
+      id = id.split(this.cookieSeparator, 2)
+      id = id[id.length - 1]
+    }
+    return id
+  }
+
+  static _readCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([.$?*|{}()\[\]\\\/+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
   }
 }
