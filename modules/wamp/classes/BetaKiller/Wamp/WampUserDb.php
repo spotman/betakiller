f5563a5566d778631d6ca982f795c9ef991e8763
psa <psaproxy@gmail.com>
2018-10-02 16:26:15 +0300

    #71 working on WAMP + ReactPHP

--- modules/wamp/classes/BetaKiller/Wamp/WampUserDb.php
+++ modules/wamp/classes/BetaKiller/Wamp/WampUserDb.php
@@ -56,7 +56,9 @@ class WampUserDb implements WampCraUserDbInterface
         return [
             'authid' => $authid,
             'key'    => $session->get('user_agent'),
-            'salt'   => '123123',//todo can be used?
+//            'salt'   => Utils::getUniqueId(),//todo can be used?
+//            'salt'   => '123123',
+            'salt'   => null,
         ];
     }
 }
