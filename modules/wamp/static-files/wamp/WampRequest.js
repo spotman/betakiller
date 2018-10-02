1e37ae774aae17c063c015ecee586fb7bf4a2dba
Psaproxy <Psaproxy@users.noreply.github.com>
2018-10-02 16:37:43 +0300

    74 wamp production (#86)
    - WAMP + ReactPHP
    - fix request HTTP_USER_AGENT key from server params

---
+++ modules/wamp/static-files/wamp/WampRequest.js
@@ -0,0 +1,61 @@
+'use strict';
+
+class WampRequest {
+  constructor(connection, resourcePrefix = '') {
+    this.connection     = connection
+    this.resourcePrefix = resourcePrefix
+  }
+
+  request(resource, method = '', data = undefined) {
+    let url = []
+    if (this.resourcePrefix) url.push(this.resourcePrefix)
+    url.push(resource)
+    if (method) url.push(method)
+    url = url.join('.')
+
+    data = this._normalizeCallData(data)
+
+    this.connection
+      .getSession()
+      .call(url, data)
+      .then(response => this._onResolve(response))
+      .catch(message => this._onReject(url, message))
+
+    return new Promise((resolve, reject) => {
+      this.resolve = resolve
+      this.reject  = reject
+    })
+  }
+
+  _onResolve(response) {
+    this.resolve(response)
+  }
+
+  _onReject(url, message) {
+    this.reject({'url': url, 'error': message,})
+  }
+
+  _normalizeCallData(data) {
+    if (data === null || data === undefined) {
+      return data
+    }
+
+    if (data instanceof Array) {
+      return data
+    }
+
+    if (typeof data === 'object') {
+      data = this._objectToArray(data)
+    } else {
+      data = [data]
+    }
+
+    return data
+  }
+
+  _objectToArray(data) {
+    return Object.keys(data).map(function (key) {
+      return data[key]
+    })
+  }
+}
