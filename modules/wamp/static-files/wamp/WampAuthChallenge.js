cf008e496895746335319a3418c5da7f4292bca6
psa <psaproxy@gmail.com>
2018-10-03 13:39:00 +0300

    #71 working on WAMP + ReactPHP

--- modules/wamp/static-files/wamp/WampAuthChallenge.js
+++ modules/wamp/static-files/wamp/WampAuthChallenge.js
@@ -16,16 +16,10 @@ class WampAuthChallenge {
   }
 
   run(session, method, extra) {
-    switch (method) {
-      case this.method:
-        return this._run(session, method, extra);
-
-      default:
-        throw new Error('Unknown method "' + method + '". Valid method "' + this.method + '"');
+    if (method !== this.method) {
+      throw new Error('Unknown method: ' + method + '. Valid method: ' + this.method + '.');
     }
-  }
 
-  _run(session, method, extra) {
     var key = this.secretKey;
     if (typeof extra.salt !== 'undefined') {
       key = autobahn.auth_cra.derive_key(key, extra.salt);
--- modules/wamp/static-files/wamp/WampConnection.js
+++ modules/wamp/static-files/wamp/WampConnection.js
@@ -5,117 +5,107 @@ class WampConnection {
     this.url           = url;
     this.realm         = realm;
     this.authChallenge = authChallenge;
+    this._markAsNotReady();
+  }
+
+  _markAsNotReady() {
+    this.wampConnection  = null;
+    this.wampSession     = null;
+    this.connectionReady = false;
+    this.connectionFirst = true;
+  }
+
+  _markAsReady() {
+    this.connectionFirst = !this.connectionReady;
+    this.connectionReady = true;
+  }
+
+  _ErrorNotReady() {
+    return new Error('Connection not ready. Use connect() or wait for connection complete.');
   }
 
   isOnProgress() {
-    //todo
-    return this.hasOwnProperty('wampConnection');
+    return this.wampConnection && !this.connectionReady;
   }
 
   isReady() {
-    return this.hasOwnProperty('connectionReady');
+    return this.connectionReady;
   }
 
   isFirst() {
     if (!this.isReady()) {
-      throw this._onNotReady();
+      throw this._ErrorNotReady();
     }
     return this.connectionFirst;
   }
 
   getConnection() {
-    if (!this.isOnProgress()) {
-      throw this._onNotReady();
+    if (!this.isReady()) {
+      throw this._ErrorNotReady();
     }
     return this.wampConnection;
   }
 
   getSession() {
     if (!this.isReady()) {
-      throw this._onNotReady();
+      throw this._ErrorNotReady();
     }
     return this.wampSession;
   }
 
-  close(reason, message) {
-    this.wampConnection.close(reason, message);
-    return this;
-  }
-
   connect() {
-    if (this.isOnProgress() || this.isReady()) {
-      throw this._onProgress();
+    if (this.isOnProgress()) {
+      throw new Error('Connection on progress.');
     }
-    this._connect();
-    return new Promise((resolve, reject) => {
-      this.resolve = resolve;
-      this.reject  = reject;
+    if (this.isReady()) {
+      throw new Error('Connection already ready.');
+    }
+    return new Promise(() => {
+      let options = {
+        url:   this.url,
+        realm: this.realm,
+      };
+      if (this.authChallenge instanceof WampAuthChallenge) {
+        options.authmethods = [this.authChallenge.getMethod()];
+        options.authid      = this.authChallenge.getAuthId();
+        options.onchallenge = (...args) => this._onChallenge.apply(this, args);
+      }
+      this.wampConnection         = new autobahn.Connection(options);
+      this.wampConnection.onopen  = (...args) => this._onOpen.apply(this, args);
+      this.wampConnection.onclose = (...args) => this._onClose.apply(this, args);
+      this.wampConnection.open();
     });
-  }
-
-  _connect() {
-    let options = {
-      url:   this.url,
-      realm: this.realm,
-    };
-    if (this.authChallenge instanceof WampAuthChallenge) {
-      options.authmethods = [this.authChallenge.getMethod()];
-      options.authid      = this.authChallenge.getAuthId();
-      options.onchallenge = (...args) => this._onChallenge.apply(this, args);
-    }
-    this.wampConnection         = new autobahn.Connection(options);
-    this.wampConnection.onopen  = (...args) => this._onOpen.apply(this, args);
-    this.wampConnection.onclose = (...args) => this._onClose.apply(this, args);
-    this.wampConnection.open();
-  }
-
-  _markConnectionAsReady() {
-    this.connectionFirst = !this.hasOwnProperty('connectionReady');
-    this.connectionReady = true;
-  }
-
-  _onNotReady() {
-    return new Error('Connection not ready. Use connect() or wait for connection complete.');
-  }
-
-  _onProgress() {
-    return new Error('Connection in progress or already ready.');
   }
 
   _onResolve() {
     this.resolve(this);
   }
 
-  _onReject(message) {
-    this.reject(message);
-  }
-
   _onOpen(session/*, details*/) {
     this.wampSession = session;
-    this._markConnectionAsReady();
+    this._markAsReady();
     this._onResolve();
   }
 
-  _onClose(status, reason) {
-    if (status !== 'closed') return;
-    if (!reason) {
-      reason = 'unknown';
-    } else {
-      reason = reason.reason;
+  _onChallenge(session, method, extra) {
+    try {
+      return this.authChallenge.run(session, method, extra);
+    } catch (e) {
+      throw new Error('Unable authenticate. Error: ' + e.message);
     }
-    this._onReject('Status "' + status + '". Reason "' + reason + '".');
   }
 
-  _onChallenge(session, method, extra) {
-    if (this.authChallenge instanceof WampAuthChallenge) {
-      try {
-        return this.authChallenge.run(session, method, extra);
-      } catch (e) {
-        let message = 'Authenticate challenge error: ' + e.message;
-        this._onReject(message);
-      }
-    } else {
-      throw new Error('Authenticate challenge not found or invalid instance. Valid instance "WampAuthChallenge".');
+  disconnect(reason, message) {
+    if (!this.isReady()) {
+      throw this._ErrorNotReady();
     }
+    this.wampConnection.close(reason, message);
+    return this;
+  }
+
+  _onClose(status, reason) {
+    if (status === 'lost') return;
+    this._markAsNotReady();
+    throw new Error('Connection closed. Status: ' + status + '. Reason: ' + reason);
   }
 }
