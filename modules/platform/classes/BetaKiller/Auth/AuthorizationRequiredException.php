5ba3e63d5c5108116e30037474d5f5bf40042fec
Spotman <i.am@spotman.ru>
2018-10-25 22:10:20 +0300

    Possible fix for an i18n auth issue

--- modules/platform/classes/BetaKiller/Auth/AuthorizationRequiredException.php
+++ modules/platform/classes/BetaKiller/Auth/AuthorizationRequiredException.php
@@ -32,4 +32,17 @@ class AuthorizationRequiredException extends HttpException
     {
         return true;
     }
+
+    /**
+     * Returns default message for current exception
+     * Allows throwing concrete exception without message
+     * Useful for custom exception types
+     * Return null if no default message allowed
+     *
+     * @return string
+     */
+    public function getDefaultMessageI18nKey(): ?string
+    {
+        return 'error.auth.required';
+    }
 }
