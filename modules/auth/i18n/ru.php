3272b2c88e6f5aa4f760a9fbca7930af554bbd94
Spotman <i.am@spotman.ru>
2018-10-26 14:39:44 +0300

    Move auth-related stuff to auth module + refactoring and cleanup

--- modules/auth/i18n/ru.php
+++ modules/auth/i18n/ru.php
@@ -4,9 +4,6 @@
  * Automatically generated from previous translation file.
  */
 return [
-    ':error'                                                    => ':error',
-    'The requested view :file could not be found'               => 'Запрашиваемый view-файл :file не найден',
-    ':error [ :query ]'                                         => ':error [ :query ]',
     'error.auth.user_not_exists'                                => 'Неверный логин или пароль',
     'error.auth.inactive'                                       => 'Ваш аккаунт отключён, обратитесь к администратору',
     'error.auth.incorrect_password'                             => 'Неверный логин или пароль',
