e28970b54a54b096f443f8bf97ba08fb302e628b
Spotman <i.am@spotman.ru>
2018-10-30 15:57:52 +0300

    Fix exception handling

--- modules/helper/classes/BetaKiller/Helper/LoggerHelperTrait.php
+++ modules/helper/classes/BetaKiller/Helper/LoggerHelperTrait.php
@@ -1,23 +1,29 @@
 <?php
 namespace BetaKiller\Helper;
 
+use BetaKiller\Log\Logger;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
+use Throwable;
 
 trait LoggerHelperTrait
 {
-    final protected function logException(LoggerInterface $logger, \Throwable $e, string $message = null): void
-    {
-        if ($message) {
-            $message .= ': '.$e->getMessage();
-        } else {
-            $message = $e->getMessage();
+    final protected function logException(
+        LoggerInterface $logger,
+        Throwable $e,
+        ServerRequestInterface $request = null
+    ): void {
+        $data = [
+            ':message'                    => $e->getMessage(),
+            ':file'                       => $e->getFile(),
+            ':line'                       => $e->getLine(),
+            Logger::CONTEXT_KEY_EXCEPTION => $e,
+        ];
+
+        if ($request) {
+            $data[Logger::CONTEXT_KEY_REQUEST] = $request;
         }
 
-        $logger->alert(':message at :file::line', [
-            ':message'  => $message,
-            ':file'     => $e->getFile(),
-            ':line'     => $e->getLine(),
-            'exception' => $e,
-        ]);
+        $logger->alert(':message at :file::line', $data);
     }
 }
