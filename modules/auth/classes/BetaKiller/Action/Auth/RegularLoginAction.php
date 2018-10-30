9fdd93e2a7d6e4015753f996885ce5474080550a
psa <psaproxy@gmail.com>
2018-10-30 16:15:19 +0300

    remove trailing slash

--- modules/auth/classes/BetaKiller/Action/Auth/RegularLoginAction.php
+++ modules/auth/classes/BetaKiller/Action/Auth/RegularLoginAction.php
@@ -23,7 +23,7 @@ use Psr\Http\Message\ServerRequestInterface;
  */
 class RegularLoginAction extends AbstractAction
 {
-    public const URL = LoginIFace::URL.'regular/';
+    public const URL = LoginIFace::URL.'regular';
 
     /**
      * @var \BetaKiller\Auth\AuthFacade
