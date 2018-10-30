7d5d9a31b97d1c7a3557f6de0d741a8e3327ef22
Spotman <i.am@spotman.ru>
2018-10-30 20:05:55 +0300

    Change circular href value to "#" coz of CSP inline-script violation

--- modules/iface/config/app.php
+++ modules/iface/config/app.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'url'   =>  [
-        'is_trailing_slash_enabled' =>  FALSE,
-        'circular_link_href' =>  'javascript:void(0)',
+    'url' => [
+        'is_trailing_slash_enabled' => false,
+        'circular_link_href'        => '#',
     ],
 
-    'cache' =>  [
-        'page' =>  [
-            'enabled'   =>  false,
-            'path'      =>  'cache'.DIRECTORY_SEPARATOR.'page'
-        ]
+    'cache' => [
+        'page' => [
+            'enabled' => false,
+            'path'    => 'cache'.DIRECTORY_SEPARATOR.'page',
+        ],
     ],
 ];
