81999fe6f6b97e84e745ed1067721e6dc3dd80db
Spotman <i.am@spotman.ru>
2018-11-01 18:57:46 +0300

    Update roles config and import

--- modules/platform/config/roles.php
+++ modules/platform/config/roles.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 use BetaKiller\Model\RoleInterface;
 
 return [
-    RoleInterface::ROOT_ROLE_NAME,
-    RoleInterface::DEVELOPER_ROLE_NAME,
-    RoleInterface::MODERATOR_ROLE_NAME,
-    RoleInterface::LOGIN_ROLE_NAME,
-    RoleInterface::GUEST_ROLE_NAME,
+    RoleInterface::ROOT_ROLE_NAME      => 'Root role, inherit all other roles permissions',
+    RoleInterface::DEVELOPER_ROLE_NAME => 'Role for developers',
+    RoleInterface::MODERATOR_ROLE_NAME => 'Role for moderators',
+    RoleInterface::LOGIN_ROLE_NAME     => 'Grants access to login',
+    RoleInterface::GUEST_ROLE_NAME     => 'All guests and unauthorized users',
 
-    RoleInterface::ADMIN_PANEL_ROLE_NAME,
+    RoleInterface::ADMIN_PANEL_ROLE_NAME => 'Grants access to the admin panel',
 ];
