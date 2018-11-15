223a7cb57eb948f073999c8d93c07e76d5e6ce90
Spotman <i.am@spotman.ru>
2018-11-15 16:49:45 +0300

    Remove test i18n keys

--- modules/i18n/i18n/en.php
+++ modules/i18n/i18n/en.php
@@ -3,7 +3,4 @@ declare(strict_types=1);
 
 return [
     'notification.translator.i18n.new-keys.subj' => 'New translation keys are waiting for you :)',
-    'notification.translator.i18n.new-keys.subj30000' => 'New translation keys are waiting for you :)',
-    'notification.translator.i18n.new-keys.subj40000' => 'New translation keys are waiting for you :)',
-    'notification.translator.i18n.new-keys.subj50000' => 'New translation keys are waiting for you :)',
 ];
