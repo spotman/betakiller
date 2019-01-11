49f523618dd74257fb29fa80dea182fdee49ee44
Spotman <i.am@spotman.ru>
2019-01-11 20:46:31 +0400

    Fix CompositeArgumentDefinition

--- modules/defence/classes/Spotman/Defence/CompositeArgumentDefinition.php
+++ modules/defence/classes/Spotman/Defence/CompositeArgumentDefinition.php
@@ -32,4 +32,19 @@ class CompositeArgumentDefinition extends AbstractArgumentDefinition implements 
     {
         return $this->children;
     }
+
+    /**
+     * Count elements of an object
+     *
+     * @link  https://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count(): int
+    {
+        return \count($this->children);
+    }
 }
