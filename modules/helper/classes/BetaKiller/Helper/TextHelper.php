2ffd573ff5fcbac8bab8d336fc21a2dc29c26106
Spotman <i.am@spotman.ru>
2019-01-31 15:17:54 +0300

    Fix utf8-to-ascii convert

--- modules/helper/classes/BetaKiller/Helper/TextHelper.php
+++ modules/helper/classes/BetaKiller/Helper/TextHelper.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace BetaKiller\Helper;
 
+use Transliterator;
+
 class TextHelper
 {
     /**
@@ -11,9 +13,15 @@ class TextHelper
      * @return string
      * @see http://php.net/manual/ru/function.iconv.php#105507
      * @see http://php.net/manual/ru/function.mb-convert-encoding.php#80620
+     * @see https://stackoverflow.com/a/35178045
      */
     public static function utf8ToAscii(string $utf8): string
     {
-        return \mb_convert_encoding($utf8, 'ASCII');
+        $trans = Transliterator::createFromRules(
+            ':: Any-Latin; :: Latin-ASCII; :: NFD; :: [:Nonspacing Mark:] Remove; :: Lower(); :: NFC;',
+            Transliterator::FORWARD
+        );
+
+        return $trans->transliterate($utf8);
     }
 }
