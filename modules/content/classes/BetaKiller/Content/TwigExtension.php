10b11fa8cd47793c521576baddd9d54e4ee3c4f8
Spotman <i.am@spotman.ru>
2019-02-21 17:21:56 +0300

    Remove direct container call in content twig extension

--- modules/content/classes/BetaKiller/Content/TwigExtension.php
+++ modules/content/classes/BetaKiller/Content/TwigExtension.php
@@ -2,21 +2,33 @@
 namespace BetaKiller\Content;
 
 use BetaKiller\Content\Shortcode\ShortcodeFacade;
-use BetaKiller\DI\Container;
 use BetaKiller\View\IFaceView;
 
 class TwigExtension extends \Twig_Extension
 {
+    /**
+     * @var \BetaKiller\Content\Shortcode\ShortcodeFacade
+     */
+    private $facade;
+
+    /**
+     * TwigExtension constructor.
+     *
+     * @param \BetaKiller\Content\Shortcode\ShortcodeFacade $facade
+     */
+    public function __construct(ShortcodeFacade $facade)
+    {
+        $this->facade = $facade;
+    }
+
     public function getFilters()
     {
-        $shortcodeFacade = Container::getInstance()->get(ShortcodeFacade::class);
-
         return [
 
-            new \Twig_Filter('shortcodes', function (array $context, string $text) use ($shortcodeFacade) {
+            new \Twig_Filter('shortcodes', function (array $context, string $text) {
                 $request = $context[IFaceView::REQUEST_KEY];
 
-                return $shortcodeFacade->process($text, $request, $context);
+                return $this->facade->process($text, $request, $context);
             }, ['needs_context' => true, 'is_safe' => ['html']]),
 
         ];
