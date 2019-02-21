f949b08c3fa3bfed2091d77c0d00ae9aeac4917d
Spotman <i.am@spotman.ru>
2019-02-21 18:33:56 +0300

    Simplify php exceptions ACL

--- modules/error/classes/BetaKiller/Acl/Resource/PhpExceptionResource.php
+++ modules/error/classes/BetaKiller/Acl/Resource/PhpExceptionResource.php
@@ -5,9 +5,6 @@ use BetaKiller\Model\RoleInterface;
 
 class PhpExceptionResource extends AbstractEntityRelatedAclResource
 {
-    public const PERMISSION_LIST_RESOLVED   = 'listResolved';
-    public const PERMISSION_LIST_UNRESOLVED = 'listUnresolved';
-    public const PERMISSION_LIST_IGNORED    = 'listIgnored';
     public const PERMISSION_RESOLVE         = 'resolve';
     public const PERMISSION_IGNORE          = 'ignore';
     public const PERMISSION_TEST            = 'test';
@@ -43,15 +40,7 @@ class PhpExceptionResource extends AbstractEntityRelatedAclResource
                 RoleInterface::DEVELOPER,
             ],
 
-            self::PERMISSION_LIST_RESOLVED => [
-                RoleInterface::DEVELOPER,
-            ],
-
-            self::PERMISSION_LIST_UNRESOLVED => [
-                RoleInterface::DEVELOPER,
-            ],
-
-            self::PERMISSION_LIST_IGNORED => [
+            self::ACTION_LIST => [
                 RoleInterface::DEVELOPER,
             ],
 
@@ -76,9 +65,6 @@ class PhpExceptionResource extends AbstractEntityRelatedAclResource
     protected function getActionsWithoutEntity(): array
     {
         return array_merge(parent::getActionsWithoutEntity(), [
-            self::PERMISSION_LIST_RESOLVED,
-            self::PERMISSION_LIST_UNRESOLVED,
-            self::PERMISSION_LIST_IGNORED,
             self::PERMISSION_TEST,
             self::PERMISSION_THROW,
         ]);
