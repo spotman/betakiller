70624247ec87d528c55868a234ac9f920cc005d2
Spotman <i.am@spotman.ru>
2019-02-27 11:18:19 +0300

    Allow --debug option for all minion tasks including migrations

--- application/classes/Minion/Task.php
+++ application/classes/Minion/Task.php
@@ -12,6 +12,9 @@ abstract class Minion_Task extends Kohana_Minion_Task
         // Migrations are executed by deployer with --stage option
         $this->_options['stage'] = 'development';
 
+        // Migrations can be executed with --debug option
+        $this->_options['debug'] = null;
+
         parent::__construct();
     }
 
