9925e8c33f527969f31faf001177a9161f76a771
Spotman <i.am@spotman.ru>
2019-03-08 16:33:49 +0300

    Add automated ValidationException processing

--- modules/error/classes/BetaKiller/Middleware/ExpectedExceptionMiddleware.php
+++ modules/error/classes/BetaKiller/Middleware/ExpectedExceptionMiddleware.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace BetaKiller\Middleware;
 
+use BetaKiller\Exception\PublicException;
 use BetaKiller\Exception\RedirectException;
+use BetaKiller\Exception\ValidationException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -31,6 +33,10 @@ class ExpectedExceptionMiddleware implements MiddlewareInterface
             $url      = $e->getLocation();
 
             return new RedirectResponse($url, $httpCode);
+        } catch (ValidationException $e) {
+            $message = $e->getFirstItem()->getMessage();
+
+            throw new PublicException($message);
         } catch (\Throwable $e) {
             // Rethrow other exceptions
             throw $e;
