5fa73ea30c20ca07303c6ce6ed48951adfd1dba8
Spotman <i.am@spotman.ru>
2019-03-11 19:57:06 +0300

    Fix auth widget + update auth static files

--- modules/auth/static-files/auth/webpack.config.js
+++ modules/auth/static-files/auth/webpack.config.js
@@ -3,9 +3,6 @@
 //const path = require('path');
 //const root = path.resolve(__dirname);
 const Encore = require('@symfony/webpack-encore');
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-//const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 process.noDeprecation = true;
 
@@ -18,19 +15,15 @@ Encore
   // the public path used by the web server to access the previous directory
   .setPublicPath('/assets/static/auth/dist')
 
-  .setManifestKeyPrefix('dist/')
+  .setManifestKeyPrefix('auth/dist/')
 
   // will create public/build/app.js and public/build/app.css
   .addEntry('auth', './src/auth.js')
 
-  .createSharedEntry('vendor', [
-    'jquery',
-    '@babel/polyfill',
-    '@fortawesome/fontawesome-free',
-  ])
-
   // allow sass/scss files to be processed
   .enableSassLoader()
+
+  .enableSingleRuntimeChunk()
 
   .enableSourceMaps(!Encore.isProduction())
 
@@ -49,16 +42,6 @@ Encore
  * @link https://github.com/symfony/webpack-encore/issues/139#issuecomment-323585179
  */
 const webpackConfig = Encore.getWebpackConfig();
-
-//if (Encore.isProduction()) {
-//  // Remove the old version first
-//  webpackConfig.plugins = webpackConfig.plugins.filter(
-//    plugin => !(plugin instanceof webpack.optimize.UglifyJsPlugin)
-//  );
-//
-//// Add the new one
-//  webpackConfig.plugins.push(new UglifyJsPlugin());
-//}
 
 // export the final configuration
 module.exports = webpackConfig;
