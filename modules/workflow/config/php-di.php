333f23ce87d92d494f478ed2cd460f0d508cfc53
Spotman <i.am@spotman.ru>
2019-08-08 12:17:06 +0300

    Add DI binding for StatusWorkflowInterface

--- modules/workflow/config/php-di.php
+++ modules/workflow/config/php-di.php
@@ -3,10 +3,13 @@ declare(strict_types=1);
 
 use BetaKiller\Config\WorkflowConfig;
 use BetaKiller\Config\WorkflowConfigInterface;
+use BetaKiller\Workflow\StatusWorkflow;
+use BetaKiller\Workflow\StatusWorkflowInterface;
 use function DI\autowire;
 
 return [
     'definitions' => [
         WorkflowConfigInterface::class => autowire(WorkflowConfig::class),
+        StatusWorkflowInterface::class => autowire(StatusWorkflow::class),
     ],
 ];
