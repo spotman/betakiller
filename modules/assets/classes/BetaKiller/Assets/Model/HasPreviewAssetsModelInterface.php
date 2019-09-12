e3b7aa495dd26915cb917557a948cb470a2cea7e
Spotman <i.am@spotman.ru>
2019-09-12 18:11:20 +0300

    Update assets preview data format

--- modules/assets/classes/BetaKiller/Assets/Model/HasPreviewAssetsModelInterface.php
+++ modules/assets/classes/BetaKiller/Assets/Model/HasPreviewAssetsModelInterface.php
@@ -9,6 +9,6 @@ interface HasPreviewAssetsModelInterface extends AssetsModelInterface
     // Dimensions values delimiter
     public const SIZE_DELIMITER = 'x';
 
-    public const API_KEY_PREVIEW_URL      = 'preview';
+    public const API_KEY_PREVIEW_URL      = 'preview_url';
     public const API_KEY_ALL_PREVIEWS_URL = 'previews';
 }
