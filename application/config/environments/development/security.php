7005a3707949850e24db30dc8fa071be779fc11c
Spotman <i.am@spotman.ru>
2019-10-18 20:29:09 +0300

    Add IDEA links to CSP rules for dev env

---
+++ application/config/environments/development/security.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'csp' => [
+        'rules'     => [
+            'connect' => [
+                'http://localhost:63342',   // IDEA IDE link
+                'https://localhost:63342',  // IDEA IDE link
+            ],
+        ],
+    ],
+];
