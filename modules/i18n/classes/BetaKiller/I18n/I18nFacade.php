8b717d11af114ba44fb04ed9e8cb00dafefe966a
Spotman <i.am@spotman.ru>
2019-10-29 12:07:42 +0300

    Add "+" to i18n key regex for using as i18next plural separator

--- modules/i18n/classes/BetaKiller/I18n/I18nFacade.php
+++ modules/i18n/classes/BetaKiller/I18n/I18nFacade.php
@@ -25,7 +25,7 @@ final class I18nFacade
     public const PRIMARY_LANG_ISO = 'primary';
 
     // TODO Remove underscore and replace all i18n keys
-    private const KEY_REGEX = '/^[a-z0-9_-]+(?:[\.]{1}[a-z0-9-_]+)+$/m';
+    private const KEY_REGEX = '/^[a-z0-9_-]+(?:[\.]{1}[a-z0-9-_+]+)+$/m';
 
     /**
      * @var \BetaKiller\Model\LanguageInterface[]
