693d44ea7a21521eaf91fdca2c8b7112561d9960
Spotman <i.am@spotman.ru>
2019-12-03 19:10:17 +0300

    Detect nearest allowed URL in 403 error page

--- modules/error/classes/BetaKiller/IFace/HttpError403IFace.php
+++ modules/error/classes/BetaKiller/IFace/HttpError403IFace.php
@@ -1,6 +1,68 @@
 <?php
 namespace BetaKiller\IFace;
 
-class HttpError403IFace extends AbstractHttpErrorIFace
+use BetaKiller\Helper\AclHelper;
+use BetaKiller\Helper\ServerRequestHelper;
+use BetaKiller\Url\UrlElementInterface;
+use BetaKiller\Url\UrlElementTreeInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class HttpError403IFace extends AbstractHttpErrorIFace
 {
+    /**
+     * @var \BetaKiller\Helper\AclHelper
+     */
+    private $aclHelper;
+
+    /**
+     * @var \BetaKiller\Url\UrlElementTreeInterface
+     */
+    private $tree;
+
+    /**
+     * HttpError403IFace constructor.
+     *
+     * @param \BetaKiller\Helper\AclHelper            $aclHelper
+     * @param \BetaKiller\Url\UrlElementTreeInterface $tree
+     */
+    public function __construct(AclHelper $aclHelper, UrlElementTreeInterface $tree)
+    {
+        $this->aclHelper = $aclHelper;
+        $this->tree      = $tree;
+    }
+
+    /**
+     * Returns data for View
+     * Override this method in child classes
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     *
+     * @return array
+     */
+    public function getData(ServerRequestInterface $request): array
+    {
+        $urlHelper      = ServerRequestHelper::getUrlHelper($request);
+        $allowedElement = $this->findAllowedUrlElement($request);
+
+        return array_merge(parent::getData($request), [
+            'allowed_url' => $urlHelper->makeUrl($allowedElement),
+        ]);
+    }
+
+    private function findAllowedUrlElement(ServerRequestInterface $request): UrlElementInterface
+    {
+        $stack  = ServerRequestHelper::getUrlElementStack($request);
+        $params = ServerRequestHelper::getUrlContainer($request);
+        $user   = ServerRequestHelper::getUser($request);
+
+        $current = $stack->getCurrent();
+
+        foreach ($this->tree->getReverseBreadcrumbsIterator($current) as $urlElement) {
+            if ($this->aclHelper->isUrlElementAllowed($user, $urlElement, $params)) {
+                return $urlElement;
+            }
+        }
+
+        return $this->tree->getDefault();
+    }
 }
