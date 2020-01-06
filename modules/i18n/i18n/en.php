306c218bc44827fca27c7f6d27e2b02334ed1a1e
Spotman <i.am@spotman.ru>
2020-01-06 11:13:30 +0300

    Fix missing i18n for notification group

--- modules/i18n/i18n/en.php
+++ modules/i18n/i18n/en.php
@@ -3,4 +3,6 @@ declare(strict_types=1);
 
 return [
     'notification.translator.i18n.new-keys.subj' => 'New translation keys are waiting for you :)',
+
+    'notification-group.i18n-management' => 'I18n management',
 ];
