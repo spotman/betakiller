30da5c72be46754b51fbd7fa1083dd450219c97e
Spotman <i.am@spotman.ru>
2020-02-14 15:55:15 +0300

    Add consistence check to ORM::count_relations()

--- modules/orm/classes/Kohana/ORM.php
+++ modules/orm/classes/Kohana/ORM.php
@@ -1533,11 +1533,17 @@ class Kohana_ORM extends Model implements Serializable {
 	 */
 	public function count_relations($alias, $far_keys = NULL)
 	{
+	    $pk = $this->pk();
+
+	    if (!$pk) {
+            throw new DomainException('Missing primary key value for '.$this->table_name());
+        }
+
 		if ($far_keys === NULL)
 		{
 			return (int) DB::select(array(DB::expr('COUNT(*)'), 'records_found'))
 				->from($this->_has_many[$alias]['through'])
-				->where($this->_has_many[$alias]['foreign_key'], '=', $this->pk())
+				->where($this->_has_many[$alias]['foreign_key'], '=', $pk)
 				->execute($this->_db)->get('records_found');
 		}
 
