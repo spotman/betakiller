f9bfb70a3720d55ceb51dc4bce4d0b39677fa68c
Spotman <i.am@spotman.ru>
2020-03-11 17:34:24 +0300

    Remove underscores from i18n keys (fix missing)

--- modules/cookies-notification/i18n/en.php
+++ modules/cookies-notification/i18n/en.php
@@ -4,7 +4,7 @@
  * Automatically generated from previous translation file.
  */
 return [
-    'cookies_notification.header'  => 'Cookies used on the website!',
+    'cookies-notification.header'  => 'Cookies used on the website!',
     'cookies-notification.message' => 'This website uses cookies to ensure you get the best experience on our website.',
     'cookies-notification.dismiss' => 'Got it!',
     'cookies-notification.link'    => 'Learn more',
