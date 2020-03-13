6e5cc0ddbbf5ff96dc0673038809173e26e95076
Spotman <i.am@spotman.ru>
2020-03-13 14:50:27 +0300

    Prevent exceptions from LoggerHelper in case of broken logger

--- modules/helper/classes/BetaKiller/Helper/LoggerHelper.php
+++ modules/helper/classes/BetaKiller/Helper/LoggerHelper.php
@@ -1,6 +1,7 @@
 <?php
 namespace BetaKiller\Helper;
 
+use BetaKiller\Exception;
 use BetaKiller\Log\Logger;
 use BetaKiller\Model\UserInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -15,21 +16,29 @@ class LoggerHelper
         UserInterface $user = null,
         ServerRequestInterface $request = null
     ): void {
-        $data = [
-            ':message'                    => $e->getMessage(),
-            ':file'                       => $e->getFile(),
-            ':line'                       => $e->getLine(),
-            Logger::CONTEXT_KEY_EXCEPTION => $e,
-        ];
+        try {
+            $data = [
+                ':message'                    => $e->getMessage(),
+                ':file'                       => $e->getFile(),
+                ':line'                       => $e->getLine(),
+                Logger::CONTEXT_KEY_EXCEPTION => $e,
+            ];
 
-        if ($user) {
-            $data[Logger::CONTEXT_KEY_USER] = $user;
+            if ($user) {
+                $data[Logger::CONTEXT_KEY_USER] = $user;
+            }
+
+            if ($request) {
+                $data[Logger::CONTEXT_KEY_REQUEST] = $request;
+            }
+
+            $logger->alert(':message at :file::line', $data);
+        } catch (Throwable $error) {
+            /** @noinspection ForgottenDebugOutputInspection */
+            \error_log(Exception::oneLiner($e));
+
+            /** @noinspection ForgottenDebugOutputInspection */
+            \error_log(Exception::oneLiner($error));
         }
-
-        if ($request) {
-            $data[Logger::CONTEXT_KEY_REQUEST] = $request;
-        }
-
-        $logger->alert(':message at :file::line', $data);
     }
 }
