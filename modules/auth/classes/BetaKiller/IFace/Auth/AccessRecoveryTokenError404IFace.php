811993166fcf93de10c7f0687531a12d1bca6f70
Spotman <i.am@spotman.ru>
2021-01-21 17:01:43 +0300

    Fix 404 page for missing AccessRecovery token

--- modules/auth/classes/BetaKiller/IFace/Auth/AccessRecoveryTokenError404IFace.php
+++ modules/auth/classes/BetaKiller/IFace/Auth/AccessRecoveryTokenError404IFace.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 namespace BetaKiller\IFace\Auth;
 
 use BetaKiller\Helper\ServerRequestHelper;
+use BetaKiller\I18n\I18nFacade;
 use BetaKiller\IFace\AbstractIFace;
 use Psr\Http\Message\ServerRequestInterface;
 
 class AccessRecoveryTokenError404IFace extends AbstractIFace
 {
+    /**
+     * @var \BetaKiller\I18n\I18nFacade
+     */
+    private I18nFacade $i18n;
+
+    /**
+     * AccessRecoveryTokenError404IFace constructor.
+     *
+     * @param \BetaKiller\I18n\I18nFacade $i18n
+     */
+    public function __construct(I18nFacade $i18n)
+    {
+        $this->i18n = $i18n;
+    }
+
     /**
      * Returns data for View
      *
@@ -20,8 +36,14 @@ class AccessRecoveryTokenError404IFace extends AbstractIFace
     {
         $urlHelper = ServerRequestHelper::getUrlHelper($request);
 
+        $lang = !ServerRequestHelper::isGuest($request)
+            ? ServerRequestHelper::getUser($request)->getLanguage()
+            : $this->i18n->getPrimaryLanguage();
+
+        $params = $urlHelper->createUrlContainer()->setEntity($lang);
+
         return [
-            'request_url' => $urlHelper->makeCodenameUrl(AccessRecoveryRequestIFace::codename()),
+            'request_url' => $urlHelper->makeCodenameUrl(AccessRecoveryRequestIFace::codename(), $params),
         ];
     }
 }
