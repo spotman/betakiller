ac7b4cde2036c00b2fd742a60c1c1e2f16af3b2f
Spotman <i.am@spotman.ru>
2021-02-10 10:31:32 +0300

    Add phpDoc

--- modules/search/classes/BetaKiller/Search/SearchResultsItemInterface.php
+++ modules/search/classes/BetaKiller/Search/SearchResultsItemInterface.php
@@ -1,6 +1,12 @@
 <?php
 namespace BetaKiller\Search;
 
+/**
+ * Interface SearchResultsItemInterface
+ * Marker for objects used as a search result item
+ *
+ * @package BetaKiller\Search
+ */
 interface SearchResultsItemInterface
 {
 }
