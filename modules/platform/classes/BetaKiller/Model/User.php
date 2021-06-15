404ade739465065b80ae76aae291e9780b9221da
Spotman <i.am@spotman.ru>
2021-06-15 10:58:20 +0300

    Return missing User::$allUserRolesNames property

--- modules/platform/classes/BetaKiller/Model/User.php
+++ modules/platform/classes/BetaKiller/Model/User.php
@@ -41,6 +41,8 @@ class User extends \ORM implements UserInterface
     public const  REL_ROLES    = 'roles';
     public const  REL_SESSIONS = 'sessions';
 
+    protected $allUserRolesNames = [];
+
     protected function configure(): void
     {
         $this->_table_name       = self::TABLE_NAME;
