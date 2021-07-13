076e17537c749f65a114f88beee02273c9dd90f9
Denis Terekhov <i.am@spotman.ru>
2021-07-13 19:03:08 +0300

    Configure sending metrics to StatsD for dev env

--- application/config/environments/development/php-di.php
+++ application/config/environments/development/php-di.php
@@ -1,8 +1,7 @@
 <?php
 
 use Beberlei\Metrics\Collector\Collector;
-use Beberlei\Metrics\Collector\Logger;
-use Psr\Log\LoggerInterface;
+use Beberlei\Metrics\Collector\StatsD;
 use function DI\factory;
 
 return [
@@ -14,9 +13,12 @@ return [
 
     'definitions' => [
 
-        Collector::class => factory(function (LoggerInterface $logger) {
-            // Send metrics to Monolog
-            return new Logger($logger);
+        Collector::class => factory(function () {
+            // Send metrics to the local StatsD instance
+            $host = getenv('STATSD_HOST');
+            $port = getenv('STATSD_PORT');
+
+            return new StatsD($host, $port);
         }),
 
     ],
