032d170f5b4dc451cae1758444a49a3175fd4a18
Denis Terekhov <i.am@spotman.ru>
2023-06-30 18:59:12 +0300

    Improve debug info for SQL queries

--- modules/database/classes/Kohana/Database/Query.php
+++ modules/database/classes/Kohana/Database/Query.php
@@ -1,35 +1,37 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
+<?php defined('SYSPATH') or die('No direct script access.');
+
 /**
  * Database query wrapper.  See [Parameterized Statements](database/query/parameterized) for usage and examples.
  *
- * @package    Kohana/Database
- * @category   Query
- * @author     Kohana Team
+ * @package        Kohana/Database
+ * @category       Query
+ * @author         Kohana Team
  * @copyright  (c) 2008-2009 Kohana Team
- * @license    http://kohanaphp.com/license
+ * @license        http://kohanaphp.com/license
  */
-class Kohana_Database_Query {
+class Kohana_Database_Query
+{
 
-	// Query type
-	protected $_type;
+    // Query type
+    protected $_type;
 
-	// Execute the query during a cache hit
-	protected $_force_execute = FALSE;
+    // Execute the query during a cache hit
+    protected $_force_execute = false;
 
-	// Cache lifetime
-	protected $_lifetime = NULL;
+    // Cache lifetime
+    protected $_lifetime = null;
 
-	// SQL statement
-	protected $_sql;
+    // SQL statement
+    protected $_sql;
 
-	// Quoted query parameters
-	protected $_parameters = array();
+    // Quoted query parameters
+    protected $_parameters = [];
 
-	// Return results as associative arrays or objects
-	protected $_as_object = FALSE;
+    // Return results as associative arrays or objects
+    protected $_as_object = false;
 
-	// Parameters for __construct when using object results
-	protected $_object_params = array();
+    // Parameters for __construct when using object results
+    protected $_object_params = [];
 
     private static int $queryCounter = 0;
 
@@ -41,16 +43,16 @@ class Kohana_Database_Query {
     private static array $queries = [];
 
     /**
-	 * Creates a new SQL query of the specified type.
-	 *
-	 * @param   integer  $type  query type: Database::SELECT, Database::INSERT, etc
-	 * @param   string   $sql   query string
-	 */
-	public function __construct($type, $sql)
-	{
-		$this->_type = $type;
-		$this->_sql = $sql;
-	}
+     * Creates a new SQL query of the specified type.
+     *
+     * @param integer $type query type: Database::SELECT, Database::INSERT, etc
+     * @param string  $sql  query string
+     */
+    public function __construct($type, $sql)
+    {
+        $this->_type = $type;
+        $this->_sql  = $sql;
+    }
 
     public static function getQueryCount(): int
     {
@@ -78,229 +80,236 @@ class Kohana_Database_Query {
     }
 
     /**
-	 * Return the SQL query string.
-	 *
-	 * @return  string
-	 */
-	public function __toString()
-	{
-		try
-		{
-			// Return the SQL string
-			return $this->compile(Database::instance());
-		}
-		catch (Throwable $e)
-		{
-			return Kohana_Exception::text($e);
-		}
-	}
+     * Return the SQL query string.
+     *
+     * @return  string
+     */
+    public function __toString()
+    {
+        try {
+            // Return the SQL string
+            return $this->compile(Database::instance());
+        } catch (Throwable $e) {
+            return Kohana_Exception::text($e);
+        }
+    }
 
-	/**
-	 * Get the type of the query.
-	 *
-	 * @return  integer
-	 */
-	public function type()
-	{
-		return $this->_type;
-	}
+    /**
+     * Get the type of the query.
+     *
+     * @return  integer
+     */
+    public function type()
+    {
+        return $this->_type;
+    }
 
-	/**
-	 * Enables the query to be cached for a specified amount of time.
-	 *
-	 * @param   integer  $lifetime  number of seconds to cache, 0 deletes it from the cache
-	 * @param   boolean  $force whether or not to execute the query during a cache hit
-	 * @return  $this
-	 * @uses    Kohana::$cache_life
-	 */
-	public function cached($lifetime = NULL, $force = FALSE)
-	{
-		if ($lifetime === NULL)
-		{
-			// Use the global setting
-			$lifetime = Kohana::$cache_life;
-		}
+    /**
+     * Enables the query to be cached for a specified amount of time.
+     *
+     * @param integer $lifetime number of seconds to cache, 0 deletes it from the cache
+     * @param boolean $force    whether or not to execute the query during a cache hit
+     *
+     * @return  $this
+     * @uses    Kohana::$cache_life
+     */
+    public function cached($lifetime = null, $force = false)
+    {
+        if ($lifetime === null) {
+            // Use the global setting
+            $lifetime = Kohana::$cache_life;
+        }
 
-		$this->_force_execute = $force;
-		$this->_lifetime = $lifetime;
+        $this->_force_execute = $force;
+        $this->_lifetime      = $lifetime;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Returns results as associative arrays
-	 *
-	 * @return  $this
-	 */
-	public function as_assoc()
-	{
-		$this->_as_object = FALSE;
+    /**
+     * Returns results as associative arrays
+     *
+     * @return  $this
+     */
+    public function as_assoc()
+    {
+        $this->_as_object = false;
 
-		$this->_object_params = array();
+        $this->_object_params = [];
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Returns results as objects
-	 *
-	 * @param   string|bool $class  classname or TRUE for stdClass
-	 * @param   array   $params
-	 * @return  $this
-	 */
-	public function as_object($class = TRUE, array $params = NULL)
-	{
-		$this->_as_object = $class;
+    /**
+     * Returns results as objects
+     *
+     * @param string|bool $class classname or TRUE for stdClass
+     * @param array       $params
+     *
+     * @return  $this
+     */
+    public function as_object($class = true, array $params = null)
+    {
+        $this->_as_object = $class;
 
-		if ($params)
-		{
-			// Add object parameters
-			$this->_object_params = $params;
-		}
+        if ($params) {
+            // Add object parameters
+            $this->_object_params = $params;
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Set the value of a parameter in the query.
-	 *
-	 * @param   string   $param  parameter key to replace
-	 * @param   mixed    $value  value to use
-	 * @return  $this
-	 */
-	public function param($param, $value)
-	{
-		// Add or overload a new parameter
-		$this->_parameters[$param] = $value;
+    /**
+     * Set the value of a parameter in the query.
+     *
+     * @param string $param parameter key to replace
+     * @param mixed  $value value to use
+     *
+     * @return  $this
+     */
+    public function param($param, $value)
+    {
+        // Add or overload a new parameter
+        $this->_parameters[$param] = $value;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Bind a variable to a parameter in the query.
-	 *
-	 * @param   string  $param  parameter key to replace
-	 * @param   mixed   $var    variable to use
-	 * @return  $this
-	 */
-	public function bind($param, & $var)
-	{
-		// Bind a value to a variable
-		$this->_parameters[$param] =& $var;
+    /**
+     * Bind a variable to a parameter in the query.
+     *
+     * @param string $param parameter key to replace
+     * @param mixed  $var   variable to use
+     *
+     * @return  $this
+     */
+    public function bind($param, &$var)
+    {
+        // Bind a value to a variable
+        $this->_parameters[$param] =& $var;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Add multiple parameters to the query.
-	 *
-	 * @param   array  $params  list of parameters
-	 * @return  $this
-	 */
-	public function parameters(array $params)
-	{
-		// Merge the new parameters in
-		$this->_parameters = $params + $this->_parameters;
+    /**
+     * Add multiple parameters to the query.
+     *
+     * @param array $params list of parameters
+     *
+     * @return  $this
+     */
+    public function parameters(array $params)
+    {
+        // Merge the new parameters in
+        $this->_parameters = $params + $this->_parameters;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Compile the SQL query and return it. Replaces any parameters with their
-	 * given values.
-	 *
-	 * @param   mixed  $db  Database instance or name of instance
-	 * @return  string
-	 */
-	public function compile($db = NULL)
-	{
-		if ( ! is_object($db))
-		{
-			// Get the database instance
-			$db = Database::instance($db);
-		}
+    /**
+     * Compile the SQL query and return it. Replaces any parameters with their
+     * given values.
+     *
+     * @param mixed $db Database instance or name of instance
+     *
+     * @return  string
+     */
+    public function compile($db = null)
+    {
+        if (!is_object($db)) {
+            // Get the database instance
+            $db = Database::instance($db);
+        }
 
-		// Import the SQL locally
-		$sql = $this->_sql;
+        // Import the SQL locally
+        $sql = $this->_sql;
 
-		if ( ! empty($this->_parameters))
-		{
-			// Quote all of the values
-			$values = array_map(array($db, 'quote'), $this->_parameters);
+        if (!empty($this->_parameters)) {
+            // Quote all of the values
+            $values = array_map([$db, 'quote'], $this->_parameters);
 
-			// Replace the values in the SQL
-			$sql = strtr($sql, $values);
-		}
+            // Replace the values in the SQL
+            $sql = strtr($sql, $values);
+        }
 
-		return $sql;
-	}
+        return $sql;
+    }
 
-	/**
-	 * Execute the current query on the given database.
-	 *
-	 * @param   mixed    $db  Database instance or name of instance
-	 * @param   string   $as_object result object classname, TRUE for stdClass or FALSE for array
-	 * @param   array    $object_params result object constructor arguments
-	 * @return  Database_Result|integer   Database_Result for SELECT queries / the insert id for INSERT queries / number of affected rows for all other queries
-	 */
-	public function execute($db = NULL, $as_object = NULL, $object_params = NULL)
-	{
-		if ( ! is_object($db))
-		{
-			// Get the database instance
-			$db = Database::instance($db);
-		}
+    /**
+     * Execute the current query on the given database.
+     *
+     * @param mixed  $db            Database instance or name of instance
+     * @param string $as_object     result object classname, TRUE for stdClass or FALSE for array
+     * @param array  $object_params result object constructor arguments
+     *
+     * @return  Database_Result|integer   Database_Result for SELECT queries / the insert id for INSERT queries / number of affected rows for all other queries
+     */
+    public function execute($db = null, $as_object = null, $object_params = null)
+    {
+        if (!is_object($db)) {
+            // Get the database instance
+            $db = Database::instance($db);
+        }
 
-		if ($as_object === NULL)
-		{
-			$as_object = $this->_as_object;
-		}
+        if ($as_object === null) {
+            $as_object = $this->_as_object;
+        }
 
-		if ($object_params === NULL)
-		{
-			$object_params = $this->_object_params;
-		}
+        if ($object_params === null) {
+            $object_params = $this->_object_params;
+        }
 
-		// Compile the SQL query
-		$sql = $this->compile($db);
+        // Compile the SQL query
+        $sql = $this->compile($db);
 
-		if ($this->_lifetime !== NULL AND $this->_type === Database::SELECT)
-		{
-			// Set the cache key based on the database instance name and SQL
-			$cache_key = 'Database::query("'.$db.'", "'.$sql.'")';
+        if ($this->_lifetime !== null and $this->_type === Database::SELECT) {
+            // Set the cache key based on the database instance name and SQL
+            $cache_key = 'Database::query("'.$db.'", "'.$sql.'")';
 
-			// Read the cache first to delete a possible hit with lifetime <= 0
-			if (($result = Kohana::cache($cache_key, NULL, $this->_lifetime)) !== NULL
-				AND ! $this->_force_execute)
-			{
-				// Return a cached result
-				return new Database_Result_Cached($result, $sql, $as_object, $object_params);
-			}
-		}
+            // Read the cache first to delete a possible hit with lifetime <= 0
+            if (($result = Kohana::cache($cache_key, null, $this->_lifetime)) !== null
+                and !$this->_force_execute) {
+                // Return a cached result
+                return new Database_Result_Cached($result, $sql, $as_object, $object_params);
+            }
+        }
 
-        $startedOn = microtime(true);
+        if (self::$logQueries) {
+            $startedOn = microtime(true);
+
+            $bt  = debug_backtrace();
+            $bti = 1;
+
+            do {
+                $btr = $bt[$bti++];
+
+                $file = mb_strtolower(basename($btr['file']));
+            } while (str_contains($file, 'orm'));
+
+            $sql .= sprintf(' -- %s:%s %s::%s()', $btr['file'], $btr['line'], $btr['class'], $btr['function']);
+        }
 
         // Execute the query
-		$result = $db->query($this->_type, $sql, $as_object, $object_params);
+        $result = $db->query($this->_type, $sql, $as_object, $object_params);
 
         self::$queryCounter++;
 
-		if (isset($cache_key) AND $this->_lifetime > 0)
-		{
-			// Cache the result array
-			Kohana::cache($cache_key, $result->as_array(), $this->_lifetime);
-		}
+        if (isset($cache_key) and $this->_lifetime > 0) {
+            // Cache the result array
+            Kohana::cache($cache_key, $result->as_array(), $this->_lifetime);
+        }
 
-		if (self::$logQueries) {
+        if (self::$logQueries) {
             $endedOn = microtime(true);
 
             self::$queries[(string)$endedOn] = [
-                'query' => $sql,
+                'query'    => $sql,
                 'duration' => $endedOn - $startedOn,
             ];
         }
 
-		return $result;
-	}
+        return $result;
+    }
 
 } // End Database_Query
