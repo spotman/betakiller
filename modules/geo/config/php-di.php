4e0b506d90802955a07e356c1200269b9ae9bf35
Denis Terekhov <i.am@spotman.ru>
2023-07-18 15:34:51 +0300

    Add Yandex geo provider

--- modules/geo/config/php-di.php
+++ modules/geo/config/php-di.php
@@ -5,6 +5,7 @@ use Geocoder\Provider\Chain\Chain;
 use Geocoder\Provider\Geonames\Geonames;
 use Geocoder\Provider\GoogleMaps\GoogleMaps;
 use Geocoder\Provider\Provider;
+use Geocoder\Provider\Yandex\Yandex;
 use Psr\Http\Client\ClientInterface;
 use function DI\{factory};
 
@@ -16,6 +17,7 @@ return [
 
             $googleApiKey  = $env->getEnvVariable('GOOGLE_CLOUD_API_KEY');
             $geoNamesLogin = $env->getEnvVariable('GEONAMES_LOGIN');
+            $yandexMapsKey = $env->getEnvVariable('YANDEX_GEOCODER_API_KEY');
 
             if ($googleApiKey) {
                 $providers[] = new GoogleMaps($httpClient, null, $googleApiKey);
@@ -23,6 +25,10 @@ return [
 
             if ($geoNamesLogin) {
                 $providers[] = new Geonames($httpClient, $geoNamesLogin);
+            }
+
+            if ($yandexMapsKey) {
+                $providers[] = new Yandex($httpClient, null, $yandexMapsKey);
             }
 
             if (!$providers) {
