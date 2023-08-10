a44d002fbbb7c13bb6c9167a107b36dd1a8e5d6c
Denis Terekhov <i.am@spotman.ru>
2023-08-10 12:41:19 +0300

    Add UserResourceRulesCollector

---
+++ modules/platform/classes/BetaKiller/Acl/ResourceRulesCollector/UserResourceRulesCollector.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace BetaKiller\Acl\ResourceRulesCollector;
+
+final class UserResourceRulesCollector extends AbstractStatusRelatedResourceRulesCollector
+{
+}
