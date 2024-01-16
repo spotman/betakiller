660db30d90c88b6423d2a869f08f731c16fbead9
Denis Terekhov <i.am@spotman.ru>
2024-01-16 19:29:24 +0300

    Add i18n for Notification messages

---
+++ modules/notification/i18n/ru.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'notification.developer.test.direct.subj'    => 'Тест личного сообщения от разработчика',
+    'notification.developer.test.broadcast.subj' => 'Тест группового сообщения от разработчика',
+];
