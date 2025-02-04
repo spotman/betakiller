b85c3ce95b1656507de78bc3c86a3ba24745565a
Denis Terekhov <i.am@spotman.ru>
2025-02-04 16:50:20 +0700

    Add LogicException

---
+++ modules/core/classes/BetaKiller/Exception/LogicException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BetaKiller\Exception;
+
+use BetaKiller\Exception;
+
+class LogicException extends Exception
+{
+    public function __construct(string $message, array $variables = null)
+    {
+        parent::__construct($message, $variables, 500);
+    }
+}
