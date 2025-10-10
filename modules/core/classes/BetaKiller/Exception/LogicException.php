2eb4ca0d1e0cefa07ba1ec54c12dd40577be189c
Denis Terekhov <i.am@spotman.ru>
2025-10-10 20:05:37 +0300

    Allow previous exception to be transferred to LogicException

--- modules/core/classes/BetaKiller/Exception/LogicException.php
+++ modules/core/classes/BetaKiller/Exception/LogicException.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace BetaKiller\Exception;
 
 use BetaKiller\Exception;
+use Throwable;
 
 class LogicException extends Exception
 {
-    public function __construct(string $message = null, array $variables = null)
+    public function __construct(string $message = null, array $variables = null, Throwable $previous = null)
     {
-        parent::__construct($message ?? '', $variables, 500);
+        parent::__construct($message ?? '', $variables, 500, $previous);
     }
 }
