37c47db75bb74bbec1710855454eaea43fdfba38
Denis Terekhov <i.am@spotman.ru>
2025-10-13 17:53:04 +0300

    Add HasSearchQueryInterface for search-related repositories

---
+++ modules/ddd/classes/BetaKiller/Repository/HasSearchQueryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BetaKiller\Repository;
+
+use BetaKiller\Query\RepositoryQueryInterface;
+use BetaKiller\Search\SearchResultsInterface;
+
+interface HasSearchQueryInterface extends RepositoryInterface
+{
+    public function search(RepositoryQueryInterface $query, int $page, int $itemsPerPage = null): SearchResultsInterface;
+}
