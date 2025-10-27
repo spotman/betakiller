62ff2dfba099b609aa5050a40fbbbebfae3dd5cf
Denis Terekhov <i.am@spotman.ru>
2025-10-27 16:08:16 +0300

    Add "status code group" related helpers to HttpExceptionInterface

--- modules/core/classes/BetaKiller/Exception/HttpException.php
+++ modules/core/classes/BetaKiller/Exception/HttpException.php
@@ -1,20 +1,52 @@
 <?php
+
 declare(strict_types=1);
 
 namespace BetaKiller\Exception;
 
 use BetaKiller\Exception;
+use Throwable;
 
 class HttpException extends Exception implements HttpExceptionInterface
 {
     /**
-     * @param integer         $code the exception code
+     * @param int             $code the exception code
      * @param string|null     $message
      * @param array|null      $variables
      * @param \Throwable|null $previous
      */
-    public function __construct(int $code, string $message = null, array $variables = null, \Throwable $previous = null)
+    public function __construct(int $code, string $message = null, array $variables = null, Throwable $previous = null)
     {
         parent::__construct($message ?? '', $variables, $code, $previous);
+    }
+
+    public function isInformational(): bool
+    {
+        return $this->isCodeInRange(100, 199);
+    }
+
+    public function isSuccessful(): bool
+    {
+        return $this->isCodeInRange(200, 299);
+    }
+
+    public function isRedirection(): bool
+    {
+        return $this->isCodeInRange(300, 399);
+    }
+
+    public function isClientError(): bool
+    {
+        return $this->isCodeInRange(400, 499);
+    }
+
+    public function isServerError(): bool
+    {
+        return $this->isCodeInRange(500, 599);
+    }
+
+    private function isCodeInRange(int $from, int $to): bool
+    {
+        return $this->code >= $from && $this->code <= $to;
     }
 }
--- modules/core/classes/BetaKiller/Exception/HttpExceptionInterface.php
+++ modules/core/classes/BetaKiller/Exception/HttpExceptionInterface.php
@@ -1,8 +1,18 @@
 <?php
+
 namespace BetaKiller\Exception;
 
 use BetaKiller\ExceptionInterface;
 
 interface HttpExceptionInterface extends ExceptionInterface
 {
+    public function isInformational(): bool;
+
+    public function isSuccessful(): bool;
+
+    public function isRedirection(): bool;
+
+    public function isClientError(): bool;
+
+    public function isServerError(): bool;
 }
